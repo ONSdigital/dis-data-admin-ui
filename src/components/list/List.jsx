9b48f25d24f9242e5918841987b880e308e97ed4
jondewijones <jondewijones@gmail.com>
2024-11-11 15:04:22 +0000

    as basePath to url

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -10,7 +10,7 @@ export default function Layout({ items }) {
                     <li key={item.id} className="ons-document-list__item">
                         <div className="ons-document-list__item-content">
                             <div className="ons-document-list__item-header">
-                                <h2><a href={item.url}>{item.title}</a></h2>
+                                <h2><a href={`/data-admin/${item.url}`}>{item.title}</a></h2>
                                 <ul className="ons-document-list__item-metadata ons-u-mb-1xs">
                                     <li className="ons-document-list__item-attribute">
                                         <span className="ons-u-fw-b">ID: </span>{item.id}
