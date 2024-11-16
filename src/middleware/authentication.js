38cf01ae171428a9a41bda0747a0f83c0441ab7a
jondewijones <jondewijones@gmail.com>
2024-11-16 20:09:14 +0000

    add log and colons

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
 
 import { logInfo } from '@/utils/log/log';
 
 const publicRoutes = ['/florence/login']
 
 export async function authenticationMiddleware(req) {
-    const path = req.nextUrl.pathname
-    const isPublicRoute = publicRoutes.includes(path)
-    const cookie = (await cookies()).get('session')?.value
+    const path = req.nextUrl.pathname;
+    const isPublicRoute = publicRoutes.includes(path);
+    const cookie = (await cookies()).get('session')?.value;
     if ((!cookie || cookie === "false") && !isPublicRoute) {
-        
-        return NextResponse.redirect(new URL('/florence/login', req.nextUrl))
+        logInfo("no auth cookie detected redirecting to login screen", null, null);
+        return NextResponse.redirect(new URL('/florence/login', req.nextUrl));
     }
     return NextResponse.next();
 }
