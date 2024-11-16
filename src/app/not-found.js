536941eb10506c52c0036d815ea006c81ee64e0b
jondewijones <jondewijones@gmail.com>
2024-11-16 20:10:48 +0000

    add 404 page

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+        <>
+            <h1 className="ons-u-fs-xxxl ons-u-mt-xl">Page not found</h1>
+            <p>If you entered a web address, check it is correct.</p>
+            <p>If you pasted the web address, check you copied the whole address.</p>
+            <p>If the web address is correct, or you selected a link or button, raise a support request in slack.</p>
+        </>
+    );
+}
