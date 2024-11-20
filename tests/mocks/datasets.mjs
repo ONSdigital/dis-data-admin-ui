6a2f43fa582815185a6a8f08b02b0cb1c2e1e9f8
jondewijones <jondewijones@gmail.com>
2024-11-20 15:42:03 +0000

    add simple mocks for stubbed api

---
+++ tests/mocks/datasets.mjs
@@ -0,0 +1,21 @@
+const datasetList = {
+    "items": [
+        {
+            "title": "Test dataset",
+            "description": "Something about a test dataset",
+            "id": "test-dataset"
+        },
+        {
+            "title": "Consumer prices",
+            "description": "Something about consumer prices",
+            "id": "cpih"
+        },
+        {
+            "title": "Weekly deaths",
+            "description": "Something about weekly deaths",
+            "id": "weekly-deaths"
+        }
+    ]
+}
+
+export { datasetList }
