0f6f1af8287db32d15e8847f1b2e7e5d6acb9277
jondewijones <jondewijones@gmail.com>
2024-12-05 20:33:26 +0000

    add func for login link with redirect

--- src/utils/auth/auth.js
+++ src/utils/auth/auth.js
@@ -1,8 +1,22 @@
+
+const logoutURL = "/florence/logout"
+const loginURL = "/florence/login"
+
 /**
  * Redirect to logout endpoint in Florence
  */
 const logout = () => {
-    window.location = "/florence/logout";
+    window.location = logoutURL;
 }
 
-export { logout };
+/**
+ * Returns login URL with redirect param set
+ * @param {string} redirectPath - path to for redirect param
+ */
+const getLoginURLWithRedirect = (redirectPath) => {
+    const basePath = "/data-admin/"
+    const redirectTo = encodeURIComponent(basePath + redirectPath)
+    return `${loginURL}?redirect=${redirectTo}`
+}
+
+export { logout, getLoginURLWithRedirect };
