9cd05d2f972f6c343271a7699d691cbc6050b3e3
jondewijones <jondewijones@gmail.com>
2024-12-19 20:39:21 +0000

    consistent use of quotes

--- src/utils/auth/auth.test.js
+++ src/utils/auth/auth.test.js
@@ -2,11 +2,11 @@
 import { logout, getLoginURLWithRedirect } from "./auth";
 
 
-test('logout redirects to the correct url ', () => {
+test("logout redirects to the correct url ", () => {
     expect(logout()).toBe("/florence/logout");
 });
 
-test('getLoginURLWithRedirect returns the correct url ', () => {
+test("getLoginURLWithRedirect returns the correct url ", () => {
     expect(getLoginURLWithRedirect()).toBe("/florence/login?redirect=%2Fdata-admin%2F");
     expect(getLoginURLWithRedirect("/datasets")).toBe("/florence/login?redirect=%2Fdata-admin%2F%2Fdatasets");
 });
