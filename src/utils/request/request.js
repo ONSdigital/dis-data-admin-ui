88e750300ca2c5caad3951b39c27d8e9f99e334d
jondewijones <jondewijones@gmail.com>
2025-01-08 15:49:51 +0000

    add basic logic for adding authorisation headers to requests

--- src/utils/request/request.js
+++ src/utils/request/request.js
@@ -1,11 +1,15 @@
 import { logInfo, logError } from '../log/log';
 
+const xFlorenceHeaderKey = "X-Florence-Token";
+const authHeaderKey = "Authorization"
+
 // work in progress/place holder request func
-export default async function request(url) {
+export default async function request(cfg, url) {
     const startedAt = new Date(Date.now()).toISOString();
     logInfo("http request started", null, {requestID: "", method: "GET", path: url, statusCode: 0, startedAt: startedAt, endedAt: null})
 
-    const response = await fetch(url);
+    const headers = setHeaders(cfg.authToken)
+    const response = await fetch(cfg.baseURL + url, { headers: headers });
     
     if (response.status >= 400 ) {
         logError("http request failed", null, null, {requestID: "", method: "GET", path: url, statusCode: 0, startedAt, endedAt: null})
@@ -15,4 +19,12 @@ export default async function request(url) {
     const endedAt = new Date(Date.now()).toISOString();
     logInfo("http request completed", null, {requestID: "", method: "GET", path: url, statusCode: response.status, startedAt, endedAt: endedAt})
     return json;
+}
+
+const setHeaders = (authToken) => {
+    const headers = new Headers();
+    headers.set(xFlorenceHeaderKey, authToken)
+    headers.set(authHeaderKey, authToken)
+
+    return headers;
 }
