1843fb4145729c6f34e7d3108ef6860c7996a7ee
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-01-14 09:19:57 +0000

    .env file edit, adding back conditional to list component

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,22 +1,28 @@
 import Link from 'next/link'
 
 export default function List({ items }) {
-    return (
-        <ul className="ons-document-list">
-            {items.map((item) => (
-                <li key={item.id} className="ons-document-list__item">
-                    <div className="ons-document-list__item-content">
-                        <div className="ons-document-list__item-header">
-                            <h2><Link href={`${item.url}`}>{item.title}</Link></h2>
-                            <ul className="ons-document-list__item-metadata ons-u-mb-1xs">
-                                <li className="ons-document-list__item-attribute">
-                                    <span className="ons-u-fw-b">ID: </span>{item.id}
-                                </li>
-                            </ul>
+    if (!items || items.length === 0) {
+        return (
+            <p>No results</p>
+        )
+    } else {
+        return (
+            <ul className="ons-document-list">
+                {items.map((item) => (
+                    <li key={item.id} className="ons-document-list__item">
+                        <div className="ons-document-list__item-content">
+                            <div className="ons-document-list__item-header">
+                                <h2><Link href={`${item.url}`}>{item.title}</Link></h2>
+                                <ul className="ons-document-list__item-metadata ons-u-mb-1xs">
+                                    <li className="ons-document-list__item-attribute">
+                                        <span className="ons-u-fw-b">ID: </span>{item.id}
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </li>
-            ))}
-        </ul>
-    )
+                    </li>
+                ))}
+            </ul>
+        )
+    }
 }
