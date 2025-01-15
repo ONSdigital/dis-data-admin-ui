6a45cceda8ab3ac275c21679c8f02e688a8a4f2e
jondewijones <jondewijones@gmail.com>
2025-01-15 20:09:40 +0000

    update to use new http funcs

--- src/app/datasets/[id]/page.js
+++ src/app/datasets/[id]/page.js
@@ -1,19 +1,16 @@
 import { cookies } from "next/headers";
 
-import request from "@/utils/request/request";
+import { httpGet, SSRequestConfig } from "@/utils/request/request";
 
 import List from "../../../components/list/List"
 import { mapListItems } from './mapper';
 
 export default async function Dataset({ params }) {
-    const baseURL = process.env.API_ROUTER_URL;
-    const cookieStore = await cookies();
-    const authToken =  cookieStore.get("access_token");
-    const reqCfg = {baseURL: baseURL, authToken: authToken.value};
+    const reqCfg = await SSRequestConfig(cookies);
 
     const { id } = await params
-    let dataset = await request(reqCfg, `/datasets/${id}`)
-    let editions = await request(reqCfg, `/datasets/${id}/editions`)
+    let dataset = await httpGet(reqCfg, `/datasets/${id}`)
+    let editions = await httpGet(reqCfg, `/datasets/${id}/editions`)
 
     const listItems = mapListItems(editions.items, id)
     return (
