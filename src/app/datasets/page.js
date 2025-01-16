956afc9366075d041512fb41fc9a43de563d7533
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-01-16 12:51:04 +0000

    Added create link as list item for datasets

--- src/app/datasets/page.js
+++ src/app/datasets/page.js
@@ -10,9 +10,16 @@ export default async function Datasets() {
     const cookieStore = await cookies();
     const authToken =  cookieStore.get("access_token");
     const reqCfg = {baseURL: baseURL, authToken: authToken.value};
+
+    const listItems = [{
+        id: "Create New Dataset",
+        title: "Create",
+        url: "/datasets/create"
+    }]
+
     const data = await request(reqCfg, "/datasets");
-    const listItems = mapListItems(data.items);
-    
+    listItems.push(...mapListItems(data.items));
+
     return (
         <>
             <h1 className="ons-u-fs-xxxl">Find a dataset</h1>
