a324149882d54cd7ad05831229b0192c8e347a31
jondewijones <jondewijones@gmail.com>
2025-01-16 21:18:49 +0000

    add middleware to read server cofnig and pass to client

---
+++ src/middleware/configToClient.js
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+/**
+ * adds serverside configuration to cookies so that can be read by any client side code
+ */
+export async function configToClientMiddleware(req) {
+    const apiRouterURL = process.env.API_ROUTER_URL;
+    const cookieStore = await cookies();
+    cookieStore.set({
+        name: 'api_router_url',
+        value: apiRouterURL,
+        path: '/',
+    });
+    return NextResponse.next();
+};
