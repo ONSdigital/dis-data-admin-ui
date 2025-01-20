60706f18889c6d893bc67f250f4eee7e550c4c07
jondewijones <jondewijones@gmail.com>
2025-01-20 15:41:17 +0000

    set response status code in error log

--- src/utils/request/request.js
+++ src/utils/request/request.js
@@ -36,7 +36,7 @@ const request = async (cfg, url, method, body) => {
     const response = await fetch(cfg.baseURL + url, fetchConfig);
     
     if (response.status >= 400 ) {
-        logError("http request failed", {error: response}, {requestID: "", method: method, path: url, statusCode: 0, startedAt, endedAt: null});
+        logError("http request failed", {error: response}, {requestID: "", method: method, path: url, statusCode: response.status, startedAt, endedAt: null});
         return response;
     }
     const json = await response.json();
