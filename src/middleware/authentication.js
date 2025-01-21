c4a11e35a70a986b0ba20053a450678d7784d3f7
jondewijones <jondewijones@gmail.com>
2025-01-21 21:51:34 +0000

    fix linting warnings and errors

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,27 +1,27 @@
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-import { logInfo } from '@/utils/log/log';
-import { getLoginURLWithRedirect, validateCookie } from '@/utils/auth/auth';
+import { logInfo } from "@/utils/log/log";
+import { getLoginURLWithRedirect, validateCookie } from "@/utils/auth/auth";
 
-const publicRoutes = ['/florence/login', 'florence/logout'];
+const publicRoutes = ["/florence/login", "florence/logout"];
 
 export async function authenticationMiddleware(req) {
 
     const path = req.nextUrl.pathname;
     const isPublicRoute = publicRoutes.includes(path);
-    const logData = {path}
+    const logData = {path};
     if (isPublicRoute) {
         logInfo("authentication middleware: skipping authication. accessing public route", logData, null);
         return NextResponse.next();
     }
     
-    const cookie = (await cookies()).get('id_token')?.value
-    const isValidCookie = validateCookie(cookie)
+    const cookie = (await cookies()).get("id_token")?.value;
+    const isValidCookie = validateCookie(cookie);
     if (!cookie || !isValidCookie) {
         logInfo("authentication middleware: no auth cookies found, or invalid. redirecting to login", logData, null);
-        const logoutPath = getLoginURLWithRedirect(path)
-        return NextResponse.redirect(new URL(logoutPath, req.nextUrl))
+        const logoutPath = getLoginURLWithRedirect(path);
+        return NextResponse.redirect(new URL(logoutPath, req.nextUrl));
     }
 
     logInfo("authentication middleware: cookie found and validated", logData, null);
--- src/middleware/routeLogging.js
+++ src/middleware/routeLogging.js
@@ -1,8 +1,8 @@
-import { NextResponse } from 'next/server';
-import { logInfo } from '@/utils/log/log';
+import { NextResponse } from "next/server";
+import { logInfo } from "@/utils/log/log";
 
 
 export async function routeLoggingMiddleware(req) {
-    logInfo("route change", {url: req.url}, null)
+    logInfo("route change", {url: req.url}, null);
     return NextResponse.next();
 }
