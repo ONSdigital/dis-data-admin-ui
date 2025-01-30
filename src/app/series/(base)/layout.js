53370527d537646f5b9af6590600a3bf3bc31a26
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-01-30 14:17:02 +0000

    Changed wording on link

--- src/app/series/(base)/layout.js
+++ src/app/series/(base)/layout.js
@@ -3,7 +3,7 @@ import HeroPanel from "@/components/heroPanel/HeroPanel";
 export default function SeriesLayout({ children }) {
     return (
         <>
-            <HeroPanel hyperLink={{ text: 'Create New Dataset Series', url: 'series/create'}} title="Dataset Series" wide/>
+            <HeroPanel hyperLink={{ text: 'Add New Dataset Series', url: 'series/create'}} title="Dataset Series" wide/>
             {children}
         </>
     )
