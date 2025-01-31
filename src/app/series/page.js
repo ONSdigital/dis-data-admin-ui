e18034f0b0646b646133c4e5c25f1fee63f210c7
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-01-31 07:05:46 +0000

    Undid nested layout added div wrapper

--- src/app/series/page.js
+++ src/app/series/page.js
@@ -2,9 +2,12 @@ import { cookies } from "next/headers";
 
 import { httpGet, SSRequestConfig } from "@/utils/request/request";
 
+import HeroPanel from "@/components/heroPanel/HeroPanel"
 import Panel from "@/components/panel/Panel";
-import List from "../../../components/list/List";
+import List from "@/components/list/List";
 import { mapListItems } from "./mapper";
+
+import styles from './styles.module.css'
 
 export default async function Series() {
     const reqCfg = await SSRequestConfig(cookies);
@@ -20,12 +23,16 @@ export default async function Series() {
 
     return (
         <>
+            <div className={styles.hero}>
+                <HeroPanel hyperLink={{ text: 'Add New Dataset Series', url: 'series/create'}} title="Dataset Series" wide/>
+            </div>
+
             { error ? <Panel title="Error" variant="error">
                     <p>
                         There was an issue retrieving the list of dataset series.
                     </p>
                 </Panel> : ''}
-            <div className="ons-u-mt-l ons-u-mb-l">
+            <div className="ons-u-mt-l ons-u-mb-l test">
                 <List items={listItems}/>
             </div>
         </>
