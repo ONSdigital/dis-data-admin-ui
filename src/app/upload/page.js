c4c998b2bd7298d736842efc6a4cbfb00150a8a9
jondewijones <jondewijones@gmail.com>
2025-02-07 09:41:33 +0000

    add temporary page for upload component

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -1,8 +1,14 @@
+"use client";
+
+import ResumableFileUpload from "@/components/file-upload/ResumableFIleUpload";
+
 export default function Upload() {
     return (
         <>
             <h1>Upload (POC)</h1>
             <p>This is the temporary home for our upload component until the dataset series journey is fully mapped out/ready.</p>
+
+            <ResumableFileUpload />
         </>
     );
-}
+};
