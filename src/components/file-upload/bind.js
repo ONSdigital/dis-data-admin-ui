0c4f10d887ac19ed0807bf38454520a758a11c38
jondewijones <jondewijones@gmail.com>
2025-02-07 21:12:10 +0000

    remove commented code and unused functions

--- src/components/file-upload/bind.js
+++ src/components/file-upload/bind.js
@@ -15,7 +15,6 @@ const routerAPIURL = "http://localhost:23200/v1" //hardcoded for local testing -
 
 const bindFileUploadInput = (elementID, handleFileStart, handleFileProgress, handleFileComplete, handleError) => {
     const input = document.getElementById(elementID);
-    console.log(input);
     const r = new Resumable({
         target: `${routerAPIURL}/upload-new`,
         chunkSize: FIVE_MEGABYTES,
@@ -33,94 +32,27 @@ const bindFileUploadInput = (elementID, handleFileStart, handleFileProgress, han
             aliasName: file.container.name,
         };
         beginUploadAndUpdateComponentState(r, options, file, handleFileStart);
-        console.log("FILE ADDED!!");
     });
     r.on("fileProgress", file => {
         updateProgress(file, handleFileProgress);
-        console.log("FILE PROGRESS");
     });
-    r.on("fileError", file => {
-        handleErrorAndUpdateComponentState(file, handleError);
-        console.log("FILE ERROR")
+    r.on("fileError", (file, message) => {
+        handleError(message);
     });
     r.on("fileSuccess", file => {
-        //onSuccess(`${r.opts.query.path}/${file.relativePath}`);
-        console.log("FILE SUCCESS")
+        handleFileComplete(`${r.opts.query.path}/${file.relativePath}`);
     });
 };
 
 const beginUploadAndUpdateComponentState = (resumable, resumableOptions, file, handleFileStart) => {
-    const aliasName = file.container.name;
     resumable.opts.query = resumableOptions;
     resumable.upload();
-
-    console.log("UPLOAD RAN!!")
-    // const fileUpload = {
-    //     aliasName: aliasName,
-    //     progress: 0,
-    //     error: null,
-    //     filename: file.fileName,
-    // };
     handleFileStart()
-    //updateState(fileUpload);
 }
 
 const updateProgress = (file, handleFileProgress) => {
     const progressPercentage = Math.round(Number(file.progress() * 100));
-    // const fileUpload = {
-    //     progress: progressPercentage,
-    //     filename: file.fileName,
-    // };
     handleFileProgress(progressPercentage);
-    //updateState(fileUpload);
-}
-
-// const updateComponentState = (file, updateState) => {
-//     const progressPercentage = Math.round(Number(file.progress() * 100));
-//     const fileUpload = {
-//         progress: progressPercentage,
-//         filename: file.fileName,
-//     };
-//     updateState(fileUpload);
-// }
-
-const handleErrorAndUpdateComponentState = (file, handleError) => {
-    // const fileUpload = {
-    //     error: "An error occurred whilst uploading this file.",
-    //     filename: file.fileName,
-    // };
-
-    console.error("Error uploading file to server");
-
-    handleError("An error occurred whilst uploading this file.");
-}
-
-const handleSuccessGetFileUploadedFileInfoAndUpdateComponentState = (file, updateState, onSuccess) => {
-    const aliasName = file.resumableObj.opts.query.aliasName;
-    const reqCfg = CSRequestConfig();
-    console.log(reqCfg)
-    httpGet(`/upload/${file.uniqueIdentifier}`)
-        .then(response => {
-            const fileUpload = {
-                aliasName: aliasName,
-                progress: 0,
-                url: response.url,
-                filename: file.fileName,
-            };
-
-            updateState(fileUpload);
-
-            onSuccess(response.url);
-        })
-        .catch(error => {
-            const fileUpload = {
-                error: "An error occurred whilst uploading this file",
-                filename: file.fileName,
-            };
-
-            console.error("Error fetching uploaded file's URL: ", error);
-            updateState(fileUpload);
-        });
 }
 
 export default bindFileUploadInput;
