f1b09c97d760a11131c20576de7acbee5e5ba9d5
jondewijones <jondewijones@gmail.com>
2025-02-14 21:04:04 +0000

    update page to temporary design/WIP

--- src/app/series/[id]/page.js
+++ src/app/series/[id]/page.js
@@ -2,23 +2,33 @@ import { cookies } from "next/headers";
 
 import { httpGet, SSRequestConfig } from "@/utils/request/request";
 
-import List from "../../../components/list/List";
+import Hero from "@/components/hero/Hero";
+import List from "@/components/list/List";
 import { mapListItems } from './mapper';
 
 export default async function Dataset({ params }) {
-    const reqCfg = await SSRequestConfig(cookies);
-
     const { id } = await params;
+    const reqCfg = await SSRequestConfig(cookies);
     let dataset = await httpGet(reqCfg, `/datasets/${id}`);
     let editions = await httpGet(reqCfg, `/datasets/${id}/editions`);
 
     const listItems = mapListItems(editions.items, id);
     return (
         <>
-            <h1 className="ons-u-fs-xxxl">Dataset</h1>
-            <div className="ons-u-fs-m ons-u-mt-s ons-u-pb-xxs" style={{"color":"#707071"}}>{dataset.title}</div>
-            <p>Select an edition to view or edit.</p>
-            <List items={listItems}></List>
+            <Hero hyperLink={{ text: "Add new dataset edition", url: "/create" }} title={dataset.title} wide />           
+            <div className="ons-grid ons-u-mt-xl">
+                <div className="ons-grid__col ons-col-6@m">
+                    <h2 className="ons-u-mt-m@xxs@m">Available editions</h2>
+                    <List items={listItems}></List>
+                </div>
+                <div className="ons-grid__col ons-col-6@m ">
+                    <h2 className="ons-u-mt-m@xxs@m">ID</h2>
+                    <p>{dataset.id}</p>
+
+                    <h2 className="ons-u-mt-m@xxs@m">Summary</h2>
+                    <p>{dataset.description}</p>
+                </div>
+            </div>
         </>
     );
 }
