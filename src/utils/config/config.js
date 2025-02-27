7e29474f2915a6e688e1394e74d63a528e8f0907
jondewijones <jondewijones@gmail.com>
2025-02-27 20:40:37 +0000

    add JS doc comments

--- src/utils/config/config.js
+++ src/utils/config/config.js
@@ -1,7 +1,17 @@
+/**
+ * Map API_ROUTER_URL environment variable
+ * @param {object} envVars - environment variables
+ * @return {string} - API Router value or null
+ */
 const getAPIRouterURL = (envVars) => {
     return envVars.API_ROUTER_URL ? envVars.API_ROUTER_URL : null;
 };
 
+/**
+ * Get upload base url for upload component. 
+ * @param {object} envVars - environment variables
+ * @return {string} - empty if we're in an environment, use API_ROUTER_URL if local
+ */
 const getUploadBaseURL = (envVars) => {
     const envName = envVars.ENV_NAME;
     if (envName === "sandbox" || envName === "staging" || envName === "prod") {
@@ -11,6 +21,11 @@ const getUploadBaseURL = (envVars) => {
     }
 };
 
+/**
+ * Get app config object 
+ * @param {object} envVars - environment variables
+ * @return {object} - mapped variables for use throughout app
+ */
 const getAppConfig = (envVars) => {
     const apiRouterURL = getAPIRouterURL(envVars);
     const uploadBaseURL = getUploadBaseURL(envVars);
