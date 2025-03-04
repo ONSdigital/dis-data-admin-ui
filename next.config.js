4016996cb716327b2e880fdb662b30e0a4766e45
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-03-04 11:21:04 +0000

    Modified next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@
 module.exports = {
     output: "standalone",
     basePath: "/data-admin",
+    experimental: {
+        serverActions: {
+          allowedOrigins: ['publishing.dp.aws.onsdigital.uk'],
+        },
+      },
 };
