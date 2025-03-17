4529eaf546a81533d454740c3944f5640b0bc1cc
jondewijones <jondewijones@gmail.com>
2025-03-17 20:53:20 +0000

    add tests

---
+++ src/components/multi-content/mutliContentItems.test.js
@@ -0,0 +1,40 @@
+import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import MultiContentItems from './MultiContentItems';
+
+describe("MultiContentItems", () => {
+    it("renders correctly when type=input", () => {
+        render(<MultiContentItems id="multi-content-test" fieldType="input" buttonLabel="Add new test item"/>);
+        const input = screen.getByTestId("multi-content-test-input-0");
+        expect(input).toBeInTheDocument();
+
+        const textarea = screen.getByTestId("multi-content-test-textarea-0");
+        expect(textarea).toBeInTheDocument();
+
+        const button = screen.getByTestId("multi-content-test-add-button");
+        expect(button).toBeInTheDocument();
+    });
+
+    it("renders correctly when type=select", () => {
+        render(<MultiContentItems id="multi-content-test" fieldType="select" buttonLabel="Add new test item"/>);
+        const select = screen.getByTestId("select-multi-content-test-select-0");
+        expect(select).toBeInTheDocument();
+
+        const textarea = screen.getByTestId("multi-content-test-textarea-0");
+        expect(textarea).toBeInTheDocument();
+
+        const button = screen.getByTestId("multi-content-test-add-button");
+        expect(button).toBeInTheDocument();
+    });
+
+    it("clicking button adds new item", () => {
+        render(<MultiContentItems id="multi-content-test" fieldType="input" buttonLabel="Add new test item"/>);
+        const button = screen.getByTestId("multi-content-test-add-button");
+
+
+        expect(screen.queryByTestId("multi-content-test-input-1")).not.toBeInTheDocument();
+
+        fireEvent.click(button);
+        expect(screen.getByTestId("multi-content-test-input-1")).toBeInTheDocument();
+    });
+});
