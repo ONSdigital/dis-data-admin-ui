42f2f599782d4fc9a61c5cb5b9a5c26d60fa1862
jondewijones <jondewijones@gmail.com>
2025-04-04 09:49:04 +0100

    fix tests

--- src/components/date-time/DateTimePicker.test.js
+++ src/components/date-time/DateTimePicker.test.js
@@ -2,9 +2,13 @@ import '@testing-library/jest-dom'
 import { render, screen, fireEvent } from '@testing-library/react'
 import DateTimePicker from './DateTimePicker';
 
+renderDateTimePicker = () => {
+    render(<DateTimePicker id="release-date" dataTestId="release-date" legend="Test date and time" description=""/>);
+}
+
 describe("DateTimerPicker", () => {
     it("renders correctly", () => {
-        render(<DateTimePicker id="release-date" dataTestId="release-date"/>);
+        renderDateTimePicker();
         const day = screen.getByTestId("release-date-day");
         expect(day).toBeInTheDocument();
 
@@ -22,35 +26,35 @@ describe("DateTimerPicker", () => {
     });
 
     it("onChange handler updates day state", () => {
-        render(<DateTimePicker id="release-date" dataTestId="release-date"/>);
+        renderDateTimePicker();
         const day = screen.getByTestId("release-date-day");
         fireEvent.change(day, {target: {value: "1"}});
         expect(day.value).toBe("1");
     });
 
     it("onChange handler updates month state", () => {
-        render(<DateTimePicker id="release-date" dataTestId="release-date"/>);
+        renderDateTimePicker();
         const month = screen.getByTestId("release-date-month");
         fireEvent.change(month, {target: {value: "1"}});
         expect(month.value).toBe("1");
     });
 
     it("onChange handler updates year state", () => {
-        render(<DateTimePicker id="release-date" dataTestId="release-date"/>);
+        renderDateTimePicker();
         const year = screen.getByTestId("release-date-year");
         fireEvent.change(year, {target: {value: "2020"}});
         expect(year.value).toBe("2020");
     });
 
     it("onChange handler updates hour state", () => {
-        render(<DateTimePicker id="release-date" dataTestId="release-date"/>);
+        renderDateTimePicker();
         const hour = screen.getByTestId("release-date-hour");
         fireEvent.change(hour, {target: {value: "09"}});
         expect(hour.value).toBe("09");
     });
 
     it("onChange handler updates minutes state", () => {
-        render(<DateTimePicker id="release-date" dataTestId="release-date"/>);
+        renderDateTimePicker();
         const minutes = screen.getByTestId("release-date-minutes");
         fireEvent.change(minutes, {target: {value: "30"}});
         expect(minutes.value).toBe("30");
