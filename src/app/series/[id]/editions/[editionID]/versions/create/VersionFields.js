ac8159ea4a24878af5e7b81cba1478462f9f4db2
jondewijones <jondewijones@gmail.com>
2025-04-16 16:07:27 +0100

    move version specific fields to seperate component

---
+++ src/app/series/[id]/editions/[editionID]/versions/create/VersionFields.js
@@ -0,0 +1,76 @@
+"use client";
+
+import { useContext, useActionState, useState } from 'react';
+
+import { Button, Select, HyperLinksList } from "author-design-system-react";
+
+import { ConfigContext } from '@/context/context';
+
+import { createDatasetEdition } from '@/app/actions/createDatasetEdition';
+
+import MultiContentItems from '@/components/multi-content/MultiContentItems';
+import ResumableFileUpload from "@/components/file-upload/ResumableFileUpload";
+import DateTimePicker from '@/components/date-time/DateTimePicker';
+
+
+export default function VersionFields(props) {
+    const [qualityDesingation, setQualityDesingation] = useState("");
+
+    const appConfig = useContext(ConfigContext);
+
+    return (
+        <>
+            <h2 className="ons-u-mt-xl">Dataset version details</h2>
+            <p>The information in these fields may be copied from the most recent version.</p>
+            <div className="ons-u-mb-l">
+                <Button variants="secondary" text="Copy from previous dataset"/>
+            </div>
+
+            <DateTimePicker id="release-date" dataTestId="release-date" legend="Release date and time" description="For example, 31 3 1980" errors={props.errors}/>
+
+            <input id="quality-desingation-value" name="quality-desingation-value" type="hidden" value={qualityDesingation} />
+            <Select id="quality-desingation" 
+                label={{text: "Quality designation", description: "Something about what quality designation means"}} 
+                dataTestId="quality-desingation"
+                onChange={e => setQualityDesingation(e)} 
+                error={ (props.errors && props.errors.quality_designation) ? {id:'quality-designation-error', text: props.errors.quality_designation} : null}
+                options={[
+                    {
+                        value:"",
+                        text: "Select an option",
+                        disabled: true
+                    },
+                    {
+                        value:"accredited-official",
+                        text: "National Statistic",
+                    },
+                    {
+                        value:"official",
+                        text: "Offical Statistic",
+                    },
+                    {
+                        value:"official-in-development",
+                        text: "Offical Statistic in Development",
+                    },
+                    {
+                        value:"",
+                        text: "None",
+                    },
+                ]}
+            />
+            <h3 className="ons-u-mt-xl">Usage notes</h3>
+            <MultiContentItems id="usage-notes" fieldType="input" buttonLabel="Add new usage note" contentItems={[]}></MultiContentItems>
+
+            <h3 className="ons-u-mt-xl">Alerts</h3>
+            <MultiContentItems id="alerts" fieldType="select" buttonLabel="Add new alert" contentItems={[]}></MultiContentItems>
+
+            <h2 className="ons-u-mt-xl">Dataset file</h2>
+            <p>Select a dataset file from your local machine to upload to the Dataset Catalogue.</p>
+            <ResumableFileUpload id="dataset-upload" 
+                label="File upload" 
+                description="Click browse or drag file here" 
+                uploadBaseURL={appConfig.uploadBaseURL} 
+                validationError={(props.errors && props.errors.distributions) ? {id:'dataset-upload-error', text: props.errors.distributions} : null} />
+        </>
+    );
+}
--- src/app/series/[id]/editions/create/CreateEdition.js
+++ src/app/series/[id]/editions/create/CreateEdition.js
@@ -11,6 +11,7 @@ import { createDatasetEdition } from '@/app/actions/createDatasetEdition';
 
 import Hero from "@/components/hero/Hero";
 import Panel from "@/components/panel/Panel";
+import VersionFields from '../[editionID]/versions/create/VersionFields';
 import MultiContentItems from '@/components/multi-content/MultiContentItems';
 import ResumableFileUpload from "@/components/file-upload/ResumableFileUpload";
 import DateTimePicker from '@/components/date-time/DateTimePicker';
@@ -87,57 +88,7 @@ export default function CreateEditionForm({ datasetID }) {
                     error={ (formState.errors && formState.errors.edition_title) ? {id:'edition-title-error', text: formState.errors.edition_title} : null}
                 />
 
-                <h2 className="ons-u-mt-xl">Dataset version details</h2>
-                <p>The information in these fields may be copied from the most recent version.</p>
-                <div className="ons-u-mb-l">
-                    <Button variants="secondary" text="Copy from previous dataset"/>
-                </div>
-
-                <DateTimePicker id="release-date" dataTestId="release-date" legend="Release date and time" description="For example, 31 3 1980" errors={formState.errors}/>
-
-                <input id="quality-desingation-value" name="quality-desingation-value" type="hidden" value={qualityDesingation} />
-                <Select id="quality-desingation" 
-                    label={{text: "Quality designation", description: "Something about what quality designation means"}} 
-                    dataTestId="quality-desingation"
-                    onChange={e => setQualityDesingation(e)} 
-                    error={ (formState.errors && formState.errors.quality_designation) ? {id:'quality-designation-error', text: formState.errors.quality_designation} : null}
-                    options={[
-                        {
-                            value:"",
-                            text: "Select an option",
-                            disabled: true
-                        },
-                        {
-                            value:"accredited-official",
-                            text: "National Statistic",
-                        },
-                        {
-                            value:"official",
-                            text: "Offical Statistic",
-                        },
-                        {
-                            value:"official-in-development",
-                            text: "Offical Statistic in Development",
-                        },
-                        {
-                            value:"",
-                            text: "None",
-                        },
-                    ]}
-                />
-                <h3 className="ons-u-mt-xl">Usage notes</h3>
-                <MultiContentItems id="usage-notes" fieldType="input" buttonLabel="Add new usage note" contentItems={[]}></MultiContentItems>
-
-                <h3 className="ons-u-mt-xl">Alerts</h3>
-                <MultiContentItems id="alerts" fieldType="select" buttonLabel="Add new alert" contentItems={[]}></MultiContentItems>
-
-                <h2 className="ons-u-mt-xl">Dataset file</h2>
-                <p>Select a dataset file from your local machine to upload to the Dataset Catalogue.</p>
-                <ResumableFileUpload id="dataset-upload" 
-                    label="File upload" 
-                    description="Click browse or drag file here" 
-                    uploadBaseURL={appConfig.uploadBaseURL} 
-                    validationError={(formState.errors && formState.errors.distributions) ? {id:'dataset-upload-error', text: formState.errors.distributions} : null} />
+                <VersionFields errors={formState.errors} />
 
                 <button type="submit" className={isPending == true ? "ons-btn ons-btn ons-u-mt-l ons-btn--disabled" : "ons-btn ons-u-mt-l"} disabled={isPending}>
                     <span className="ons-btn__inner"><span className="ons-btn__text">Save new dataset edition</span></span>
