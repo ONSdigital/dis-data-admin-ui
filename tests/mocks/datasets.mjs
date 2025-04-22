c657bd96f2841469dccf922ab82fc903d3f5d89f
aryan-patel <aryanpatel1771@gmail.com>
2025-04-22 08:49:41 +0100

    move mock datasets into one array

--- tests/mocks/datasets.mjs
+++ tests/mocks/datasets.mjs
@@ -1,11 +1,6 @@
 export const datasetList = {
     "items": [
         {
-            "title": "Test dataset",
-            "description": "Something about a test dataset",
-            "id": "test-dataset"
-        },
-        {
             "title": "Consumer prices",
             "description": "Something about consumer prices",
             "id": "cpih"
@@ -14,6 +9,55 @@ export const datasetList = {
             "title": "Weekly deaths",
             "description": "Something about weekly deaths",
             "id": "weekly-deaths"
+        },
+        {
+            "description": "This is a mock dataset test description",
+            "id": "mock-quarterly",
+            "keywords": ["mock", "test"],
+            "national_statistic": "false",
+            "next_release": "TBC",
+            "release_frequency": "Quarterly",
+            "state": "published",
+            "title": "Mock Dataset",
+            "unit_of_measure": "Percentage",
+            "topics": ["Education", "Business"],
+            "contacts": [
+                {
+                    "email": "contactOne@ons.gov.uk",
+                    "name": "First Contact",
+                    "telephone": "+44 1234 567891"
+                },
+                {
+                    "email": "contactTwo@ons.gov.uk",
+                    "name": "Second Contact",
+                    "telephone": "+44 1234 567892"
+                }
+            ],
+            "last_updated": "2000-01-01T07:00:00.000Z",
+            "license": "My License",
+            "publisher": {
+                "href": "https://www.ons.gov.uk",
+                "name": "ONS"
+            },
+            "qmi": {
+                "href": "https://www.ons.gov.uk"
+            },
+            "type": "static"
+        },
+        {
+            "description": "This is a minimal mock dataset test description",
+            "id": "mock-minimal",
+            "title": "Minimal Mock Dataset",
+            "topics": ["Economy"],
+            "contacts": [
+                {
+                    "email": "contactOne@ons.gov.uk",
+                    "name": "First Contact"
+                }
+            ],
+            "last_updated": "2000-01-02T07:00:00.000Z",
+            "license": "My Minimal License",
+            "type": "static"
         }
     ]
 }
