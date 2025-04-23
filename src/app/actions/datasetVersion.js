a236b07b22a748ac30db1f887454aa66854c207a
jondewijones <jondewijones@gmail.com>
2025-04-23 21:09:22 +0100

    add dataset version action - WIP

---
+++ src/app/actions/datasetVersion.js
@@ -0,0 +1,103 @@
+'use server';
+
+import { cookies } from "next/headers";
+
+import { httpPost, SSRequestConfig } from "@/utils/request/request";
+
+import { z } from "zod";
+ 
+const versionSchema = z.object({
+    quality_designation: z.string().min(1, { message: "Quality designation is required" }),
+    release_day: z.string().min(1, { message: "Day is required" }),
+    release_month: z.string().min(1, { message: "Month is required" }),
+    release_year: z.string().min(1, { message: "Year is required" }),
+    release_hour: z.string().min(1, { message: "Hour is required" }),
+    release_minutes: z.string().min(1, { message: "Minutes are required" }),
+    distributions: z.array(z.object({
+        download_url: z.string(),
+    })).min(1, { message: "A file upload is required" })
+});
+
+// check for an error on distrubution and update error message
+// zod replies with "Required" for objects
+const addUploadFileErrorMessage = (errors) => {
+    if (!errors) {
+        return;
+    } 
+    if (errors.distributions) {
+        errors.distributions = ["File upload is required"];
+    }
+    return errors;
+};
+
+// check if any release date or time errors exists and merge them into one for time and date fieldset
+const mergeDateTimeErrors = (errors) => {
+    if (!errors) {
+        return;
+    } 
+    if (errors.release_day || errors.release_month || errors.release_year || errors.release_hour || errors.release_minutes) {
+        errors.release_date_time = ["A release time and date is required"];
+    }
+    return errors;
+};
+
+export async function datasetVersion(currentstate, formData) {
+    let actionResponse = {};
+
+    const datasetID = formData.get("dataset-id");
+    const editionID = formData.get("edition-id");
+    const usageNotes = formData.getAll("usage-notes");
+    const parsedUsageNotes = [];
+    usageNotes.map(note => {
+        if (note.title && note.note) {
+            parsedUsageNotes.push(JSON.parse(note));
+        }
+    });
+    const alerts = formData.getAll("alerts");
+    const parsedAlerts = [];
+    alerts.map(alert => {
+        if (alert.type && alert.description) {
+            parsedAlerts.push(JSON.parse(alert));
+        }
+    });
+    const datasetVersion = {
+        edition: editionID,
+        quality_designation: formData.get("quality-desingation-value"),
+        release_day: formData.get("release-date-day"),
+        release_month: formData.get("release-date-month"),
+        release_year: formData.get("release-date-year"),
+        release_hour: formData.get("release-date-hour"),
+        release_minutes: formData.get("release-date-minutes"),
+        usage_notes: parsedUsageNotes,
+        alerts: parsedAlerts,
+        distributions: [ JSON.parse(formData.get('dataset-upload-value')) ],
+        release_date: null,
+    };
+
+    const validation = versionSchema.safeParse(datasetVersion);
+
+    if (!validation.success) {
+        actionResponse.success = validation.success;
+        actionResponse.errors = validation.error.flatten().fieldErrors;
+        actionResponse.errors = addUploadFileErrorMessage(actionResponse.errors);
+        actionResponse.errors = mergeDateTimeErrors(actionResponse.errors);
+        actionResponse.submission = datasetVersion;
+        return actionResponse;
+    } 
+
+    datasetVersion.release_date =  new Date(datasetVersion.release_year, datasetVersion.release_month - 1, datasetVersion.release_day, datasetVersion.release_hour, datasetVersion.release_minutes).toISOString();
+
+    const reqCfg = await SSRequestConfig(cookies);
+    try {
+        const data = await httpPost(reqCfg, `/datasets/${datasetID}/editions/${editionID}/versions`, datasetVersion);
+        actionResponse.success = true;
+        if (data.status >= 400) {
+            actionResponse.success = false;
+            actionResponse.code = data.status;
+        } 
+    } catch (err) {
+        return err.toString();
+    }
+
+    return actionResponse;
+}
