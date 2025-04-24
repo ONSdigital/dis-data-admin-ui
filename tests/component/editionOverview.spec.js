c6f8154f18d53b6516bdb6ef647babcf1dc57b1d
jondewijones <jondewijones@gmail.com>
2025-04-24 17:34:41 +0100

    add tests for dataset edition overview page

---
+++ tests/component/editionOverview.spec.js
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+import { addValidAuthCookies } from "../utils/utils";
+
+test.describe("Dataset edition overview page", () => {
+    test("renders as expected", async ({ page, context }) => {
+        addValidAuthCookies(context);
+        await page.goto("./series/mock-quarterly/editions/time-series");
+        await expect(page.getByRole("heading", { level: 1 })).toContainText("Mock Dataset: Timeseries");
+        await expect(page.getByRole("link", { name: "Version: 1" })).toBeVisible();
+    });
+
+    test("routes to create new edition page", async ({ page, context }) => {
+        addValidAuthCookies(context);
+        await page.goto("./series/mock-quarterly/editions/time-series");
+        await page.getByRole("link", { name: "Add new dataset version" }).click();
+        await expect(page.url().toString()).toContain("series/mock-quarterly/editions/time-series/versions/create?edition_title=Timeseries");
+    });
+
+    test("routes to version overview page when selecting version from list", async ({ page, context }) => {
+        addValidAuthCookies(context);
+        await page.goto("./series/mock-quarterly/editions/time-series");
+        await page.getByRole("link", { name: "Version: 1" }).click();
+        await page.waitForURL("**/series/mock-quarterly/editions/time-series/versions/1");
+        await expect(page.url().toString()).toContain("series/mock-quarterly/editions/time-series/versions/1");
+    });
+});
