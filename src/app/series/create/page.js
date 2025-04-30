6397545dceb92e895d692f274af9efa27a8eb82c
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-04-30 10:24:05 +0100

    Added error to be shown if topics api falls over

--- src/app/series/create/page.js
+++ src/app/series/create/page.js
@@ -1,22 +1,32 @@
 import { cookies } from "next/headers";
-import { httpGet, SSRequestConfig } from "@/utils/request/request";
 
-import { createDatasetSeries } from "@/app/actions/datasetSeries"
-
+import Panel from "@/components/panel/Panel";
 import SeriesForm from "@/components/form/series/SeriesForm"
+import { createDatasetSeries } from "@/app/actions/datasetSeries"
+import { httpGet, SSRequestConfig } from "@/utils/request/request";
 
 export default async function createPage() {
 
     const reqCfg = await SSRequestConfig(cookies);
     const response = await httpGet(reqCfg, "/topics");
-    const listOfAllTopics = response.items
 
-    return (
-        <>
-            <SeriesForm 
-                listOfAllTopics={listOfAllTopics} 
-                action={createDatasetSeries}
-            />
-        </>
-    );
+    if(Object.keys(response).length === 0){
+        return(
+            <>
+                <Panel title="Topics service error" variant="error">
+                    <p>There was a problem connecting to the topics api which is required for this form. Please try again later.</p>
+                </Panel>
+            </>
+        )
+    } else {
+        const listOfAllTopics = response.items
+        return (
+            <>
+                <SeriesForm 
+                    listOfAllTopics={listOfAllTopics} 
+                    action={createDatasetSeries}
+                />
+            </>
+        );
+    }
 }
