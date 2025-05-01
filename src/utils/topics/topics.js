cab6ac56068bb15bb807898fa3779e82784974f2
aryan-patel <aryanpatel1771@gmail.com>
2025-05-01 16:37:21 +0100

    simplify convertTopicIDsToTopicTitles function

--- src/utils/topics/topics.js
+++ src/utils/topics/topics.js
@@ -2,15 +2,21 @@ import { logError } from "../log/log";
 import { httpGet } from "@/utils/request/request";
 
 export function convertTopicIDsToTopicTitles(topicIDs, reqCfg) {
+    if (!topicIDs || topicIDs.length === 0) {
+        return null;
+    }
+
     return Promise.all(
-        topicIDs.map(async (topicID) => {
-            try {
-                const response = await httpGet(reqCfg, `/topics/${topicID}`);
-                return response?.current?.title || response?.next?.title || response?.title || `${topicID} - unable to find topic title`;
-            } catch (error) {
-                logError("error fetching topic", topicID, null, error);
-                return `${topicID} - unable to find topic title`;
-            }
-        })
+        topicIDs.map((topicID) => getTopicTitle(topicID, reqCfg))
     );
+}
+
+export async function getTopicTitle(topicID, reqCfg) {
+    try {
+        const response = await httpGet(reqCfg, `/topics/${topicID}`);
+        return response?.current?.title || response?.next?.title || response?.title || `${topicID} - unable to find topic title`;
+    } catch (error) {
+        logError("error fetching topic", topicID, null, error);
+        return `${topicID} - unable to find topic title`;
+    }
 }
