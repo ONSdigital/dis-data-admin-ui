b56f412fc13b5ce9c3e96a48111cd2ad6f55b0cc
jondewijones <jondewijones@gmail.com>
2025-05-12 17:58:29 +0100

    add error handling for version GET request

--- src/app/series/[id]/editions/[editionID]/versions/[versionID]/edit/page.js
+++ src/app/series/[id]/editions/[editionID]/versions/[versionID]/edit/page.js
@@ -3,6 +3,7 @@ import { httpGet, SSRequestConfig } from "@/utils/request/request";
 
 import Hero from "@/components/hero/Hero";
 import VersionForm from "@/components/form/version/VersionForm";
+import Panel from "@/components/panel/Panel";
 
 export default async function EditVersion({ params }) {
     const { id, editionID, versionID } = await params;
@@ -11,10 +12,19 @@ export default async function EditVersion({ params }) {
     const response = await httpGet(reqCfg, `/datasets/${id}/editions/${editionID}/versions/${versionID}`);
     const version = response.next || response.current || response;
 
+    let versionError;
+    if (response.ok != null && !response.ok) {
+        versionError = true;
+    }
+
     return (
         <>
-            <Hero hyperLink={{ text: `Back to dataset version overview`, url: "../" }} title={`Edit version ${versionID}`} wide /> 
-            <VersionForm datasetID={id} editionID={editionID} />
+            { !versionError ?
+                <>
+                    <Hero hyperLink={{ text: `Back to dataset version overview`, url: "../" }} title={`Edit version ${versionID}`} wide /> 
+                    <VersionForm datasetID={id} editionID={editionID} />
+                </>
+            : <Panel title="Error" variant="error"><p>There was an issue retrieving the data for this page. Try refreshing the page.</p></Panel> }
         </>
     );
 }
