ac2074f971d3dc895514382c8639dd8bcd0a72be
jondewijones <jondewijones@gmail.com>
2025-05-14 16:32:46 +0100

    pass update action to VersionForm

--- src/app/series/[id]/editions/[editionID]/versions/[versionID]/edit/page.js
+++ src/app/series/[id]/editions/[editionID]/versions/[versionID]/edit/page.js
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers";
 import { httpGet, SSRequestConfig } from "@/utils/request/request";
 
+import { updateDatasetVersion } from "@/app/actions/datasetVersion";
+
 import Hero from "@/components/hero/Hero";
 import VersionForm from "@/components/form/version/VersionForm";
 import Panel from "@/components/panel/Panel";
@@ -22,7 +24,7 @@ export default async function EditVersion({ params }) {
             { !versionError ?
                 <>
                     <Hero hyperLink={{ text: `Back to dataset version overview`, url: "../" }} title={`Edit version ${versionID}`} wide /> 
-                    <VersionForm datasetID={id} editionID={editionID} version={version} />
+                    <VersionForm datasetID={id} editionID={editionID} version={version} action={updateDatasetVersion} />
                 </>
             : <Panel title="Error" variant="error"><p>There was an issue retrieving the data for this page. Try refreshing the page.</p></Panel> }
         </>
