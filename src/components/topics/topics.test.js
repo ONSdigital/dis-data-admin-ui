a067f3abf4843d486e012a79e88a9fdeaaff02dc
aryan-patel <aryanpatel1771@gmail.com>
2025-05-16 17:44:02 +0100

    added extra test to ensure all three possible response options work (and outdented extra space)

--- src/components/topics/topics.test.js
+++ src/components/topics/topics.test.js
@@ -3,51 +3,81 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import Topics from './Topics'
 
 describe("Topics", () => {
-        const listOfAllTopics = [
-            {
+    const listOfAllTopics = [
+        {
+            "id": "2945",
+            "current": {
                 "id": "2945",
                 "title": "Business, industry and trade",
-            },
-            {
+            }
+        },
+        {
+            "id": "3161",
+            "next": {
                 "id": "3161",
                 "title": "Census",
             }
-        ]
+        },
+        {
+            "id": "5687",
+            "title": "Employment and labour market",
+        }
+    ]
 
-        test("Topics renders props correctly", () => {
-            const selectedTopics = ['2945']
-            const setSelectedTopics = jest.fn()
+    test("Topics renders props correctly", () => {
+        const selectedTopics = ['2945']
+        const setSelectedTopics = jest.fn()
+
+        render(<Topics listOfAllTopics={listOfAllTopics} selectedTopics={selectedTopics} setSelectedTopics={setSelectedTopics}/>);
+
+        const select = screen.getByTestId('dataset-series-topics-select');
+        expect(select).toBeInTheDocument();
+
+        const list = screen.getAllByTestId("dataset-series-topics-list-item")
+        expect(list.length).toBe(1)
+
+        const options = screen.getAllByTestId("dataset-series-topics-option")
+        expect(options.length).toBe(4)
+    })
     
-            render(<Topics listOfAllTopics={listOfAllTopics} selectedTopics={selectedTopics} setSelectedTopics={setSelectedTopics}/>);
+    it("onClick handler gets called", () => {
+        const selectedTopics = ['2945']
+        const setSelectedTopics = jest.fn()
+
+        render(<Topics listOfAllTopics={listOfAllTopics} selectedTopics={selectedTopics} setSelectedTopics={setSelectedTopics}/>);
+
+        let list = screen.getAllByTestId("dataset-series-topics-list-item")
+        expect(list.length).toBe(1)
+
+        const select = screen.getByTestId('dataset-series-topics-select');
+        const button = screen.getByTestId("dataset-series-add-topic-button");
+        fireEvent.change(select, { target: { value: 3161 } })
+
+        expect(setSelectedTopics.mock.calls).toHaveLength(0);
+        fireEvent.click(button)
+        expect(setSelectedTopics).toHaveBeenCalledTimes(1)
+
+        list = screen.getAllByTestId("dataset-series-topics-list-item")
+        expect(list.length).toBe(2)
+    });
+
+    test("Dropdown list contains the expected titles", () => {
+        const selectedTopics = [];
+        const setSelectedTopics = jest.fn();
     
-            const select = screen.getByTestId('dataset-series-topics-select');
-            expect(select).toBeInTheDocument();
+        render(<Topics listOfAllTopics={listOfAllTopics} selectedTopics={selectedTopics} setSelectedTopics={setSelectedTopics}/>);
     
-            const list = screen.getAllByTestId("dataset-series-topics-list-item")
-            expect(list.length).toBe(1)
-
-            const options = screen.getAllByTestId("dataset-series-topics-option")
-            expect(options.length).toBe(3)
-        })
+        const options = screen.getAllByTestId("dataset-series-topics-option");
     
-        it("onClick handler gets called", () => {
-            const selectedTopics = ['2945']
-            const setSelectedTopics = jest.fn()
-
-            render(<Topics listOfAllTopics={listOfAllTopics} selectedTopics={selectedTopics} setSelectedTopics={setSelectedTopics}/>);
-
-            let list = screen.getAllByTestId("dataset-series-topics-list-item")
-            expect(list.length).toBe(1)
-
-            const select = screen.getByTestId('dataset-series-topics-select');
-            const button = screen.getByTestId("dataset-series-add-topic-button");
-            fireEvent.change(select, { target: { value: 3161 } })
-
-            expect(setSelectedTopics.mock.calls).toHaveLength(0);
-            fireEvent.click(button)
-            expect(setSelectedTopics).toHaveBeenCalledTimes(1)
-
-            list = screen.getAllByTestId("dataset-series-topics-list-item")
-            expect(list.length).toBe(2)
+        const expectedTitles = [
+            "Select an option",
+            "Business, industry and trade",
+            "Census",
+            "Employment and labour market"
+        ];
+    
+        options.forEach((option, index) => {
+            expect(option.textContent).toBe(expectedTitles[index]);
         });
+    });
 })
