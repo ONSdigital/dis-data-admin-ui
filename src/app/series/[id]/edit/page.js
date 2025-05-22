b3d665b09edabb371fc8b03b719b3785d8f80fa1
jondewijones <jondewijones@gmail.com>
2025-05-22 20:39:33 +0100

    handle dataset api and topic api errors

--- src/app/series/[id]/edit/page.js
+++ src/app/series/[id]/edit/page.js
@@ -11,35 +11,63 @@ export default async function createPage({params}) {
     const { id } = await params;
 
     const reqCfg = await SSRequestConfig(cookies);
-    const topicsResponse = await httpGet(reqCfg, "/topics");
-    const response = await httpGet(reqCfg, `/datasets/${id}`);
-    const dataset = response.next;
+    const topicsResp = await httpGet(reqCfg, "/topics");
+    const datasetResp = await httpGet(reqCfg, `/datasets/${id}`);
 
-    if(Object.keys(topicsResponse).length === 0){
-        return(
-            <>
-                <Panel title="Topics service error" variant="error">
-                    <p>There was a problem connecting to the topics api which is required for this form. Please try again later.</p>
-                </Panel>
-            </>
-        );
-    } else {
-        const listOfAllTopics = topicsResponse.items;
-        return (
-            <>
-                <Hero hyperLink={{ text: `Back to ${dataset.title} overview`, url: "./"}} title={`Edit ${dataset.title}`} wide/>
-                <SeriesForm 
-                    currentTitle={dataset.title} 
-                    currentID={dataset.id} 
-                    currentDescription={dataset.description} 
-                    currentTopics={dataset.topics}
-                    currentQMI={dataset.qmi?.href}
-                    currentKeywords={dataset.keywords}
-                    currentContacts={dataset.contacts}
-                    listOfAllTopics={listOfAllTopics}
-                    action={updateDatasetSeries}
-                />
-            </>
-        );
+    let datasetError, topicsError = false;
+    if (datasetResp.ok != null && !datasetResp.ok) {
+        datasetError = true;
     }
+
+    if (topicsResp.ok != null && !topicsResp.ok) {
+        topicsError = true;
+    }
+
+    const renderErrorPanel = () => {
+        if (datasetError) {
+            return (
+                <>
+                    <Panel title="Error" variant="error">
+                        <p>There was a problem retreiving data for this page. Please try again later.</p>
+                    </Panel>
+                </>
+            );
+        }
+
+        if (topicsError) {
+            return (
+                <>
+                    <Panel title="Topics service error" variant="error">
+                        <p>There was a problem connecting to the topics API which is required for this form. Please try again later.</p>
+                    </Panel>
+                </>
+            );
+        }
+        
+    }
+
+    const dataset = datasetResp?.next;
+    const listOfAllTopics = topicsResp?.items;
+    const datasetTitle = dataset?.title || id;
+    return (
+        <>
+            <Hero hyperLink={{ text: `Back to ${datasetTitle} overview`, url: "./"}} title={`Edit ${datasetTitle}`} wide/>
+            { !datasetError && !topicsError ? 
+                <> 
+                    <SeriesForm 
+                        currentTitle={dataset.title} 
+                        currentID={dataset.id} 
+                        currentDescription={dataset.description} 
+                        currentTopics={dataset.topics}
+                        currentQMI={dataset.qmi?.href}
+                        currentKeywords={dataset.keywords}
+                        currentContacts={dataset.contacts}
+                        listOfAllTopics={listOfAllTopics}
+                        action={updateDatasetSeries}
+                    />
+                </>
+                : renderErrorPanel()
+            }
+        </>
+    );
 }
