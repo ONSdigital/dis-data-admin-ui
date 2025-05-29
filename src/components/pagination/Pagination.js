fd1aa63171c8f0100e9a045f7dfe7c0990e16643
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-05-29 12:36:06 +0100

    Removed comment

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -3,7 +3,6 @@
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { Pagination } from "author-design-system-react";
 
-// pass down offset then pass it back up so page can grab what it needs?
 export default function PaginationPOC({pageTotal, currentPage, limit}){
     const { push } = useRouter();
 
