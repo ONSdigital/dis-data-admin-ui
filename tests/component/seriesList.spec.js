1e4ffb165eddcb28bf6a7173d875ae51e40e47e2
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-06-03 11:42:36 +0100

    Added component tests, amended mocked datasets

--- tests/component/seriesList.spec.js
+++ tests/component/seriesList.spec.js
@@ -20,6 +20,28 @@ test.describe("Series list page", () => {
         await expect(page.url().toString()).toContain("series/create");
     });
 
+    test("Traverse through pagination", async ({ page, context }) => {
+        addValidAuthCookies(context);
+
+        await page.goto("./series")
+        await expect(page.getByRole("link", { name: "Consumer prices" })).toBeVisible();
+        await expect(page.getByText('Page 1 of')).toBeVisible();
+        
+        await page.getByText("Next").click();
+        await expect(page.getByText('Page 2 of')).toBeVisible();
+        await expect(page.getByRole("link", { name: "Consumer prices" })).not.toBeVisible();
+        await expect(page.getByRole("link", { name: "Lorem ipsum dolor sit amet 26" })).toBeVisible();
+        
+        await page.getByText("Previous").click();
+        await expect(page.getByText('Page 1 of')).toBeVisible();
+        await expect(page.getByRole("link", { name: "Consumer prices" })).toBeVisible();
+        await expect(page.getByRole("link", { name: "Lorem ipsum dolor sit amet 26" })).not.toBeVisible();
+        
+        await page.getByRole('link', { name: 'Go to the last page (Page 5)' }).click();
+        await expect(page.getByText('Page 5 of')).toBeVisible();
+        await expect(page.getByRole("link", { name: "Lorem ipsum dolor sit amet 121" })).toBeVisible();
+    });
+
     test("Choose a fully populated dataset from a list of datasets and route to chosen dataset page", async ({ page, context }) => {
         addValidAuthCookies(context);
 
