d2ab4237788d338197753218239de4d175843631
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-06-12 12:47:51 +0100

    test url of each page from pagination component

--- src/components/pagination/pagination.test.js
+++ src/components/pagination/pagination.test.js
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom"
-import { render, screen, fireEvent } from "@testing-library/react"
+import { render, screen, within } from "@testing-library/react"
 import Pagination from "./Pagination"
 
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
@@ -9,9 +9,9 @@ useRouter.mockReturnValue({
   push: jest.fn()
 })
 
-useSearchParams.mockReturnValue({
-    toString: jest.fn()
-})
+useSearchParams.mockReturnValue(
+    ""
+)
 
 usePathname.mockReturnValue("test.com")
 
@@ -26,5 +26,22 @@ describe("Pagination", () => {
 
         const pageLinks = screen.getAllByRole("listitem")
         expect(pageLinks.length).toBe(7)
+
+        const pageOne = screen.getByTestId("pagination-1");
+        const pageOneLink = within(pageOne).getByRole("link")
+        expect(pageOneLink).toHaveAttribute("href", "test.com?limit=25&offset=0")
+
+        const pageTwo = screen.getByTestId("pagination-2");
+        const pageTwoLink = within(pageTwo).getByRole("link")
+        expect(pageTwoLink).toHaveAttribute("href", "test.com?limit=25&offset=25")
+
+        const pageFour = screen.getByTestId("pagination-4");
+        const pageFourLink = within(pageFour).getByRole("link")
+        expect(pageFourLink).toHaveAttribute("href", "test.com?limit=25&offset=75")
+
+        const pageFive = screen.getByTestId("pagination-5");
+        const pageFiveLink = within(pageFive).getByRole("link")
+        expect(pageFiveLink).toHaveAttribute("href", "test.com?limit=25&offset=100")
+
     })
 })
