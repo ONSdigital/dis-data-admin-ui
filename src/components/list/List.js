56b8a481a3c95a56945cd75a406af0ed169e1e52
jondewijones <jondewijones@gmail.com>
2025-06-19 14:10:48 +0100

    add test ids

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -9,18 +9,20 @@ export default function List({ items, noResultsText = "No results" }) {
     
     return (
         <ul className="ons-document-list">
-            {items.map((item) => (
+            {items.map((item, index) => (
                 <li key={item.id} className="ons-document-list__item">
                     <div className="ons-document-list__item-content">
                         <div className="ons-document-list__item-header">
-                            <h2 className="ons-document-list__item-title ons-u-fs-m ons-u-mt-no ons-u-mb-2xs"><Link href={`${item.url}`}>{item.title}</Link></h2>
+                            <h2 className="ons-document-list__item-title ons-u-fs-m ons-u-mt-no ons-u-mb-2xs" data-testid={`list-item-${index}-link`}>
+                                <Link href={`${item.url}`}>{item.title}</Link>
+                            </h2>
                             <ul className="ons-document-list__item-metadata ons-u-mb-2xs">
                                 <li className="ons-document-list__item-attribute">
-                                    <span className="ons-u-fw-b">ID: </span>{item.id}
+                                    <span className="ons-u-fw-b" data-testid={`list-item-${index}-id`}>ID: </span>{item.id}
                                 </li>
                                 { item.state ? 
                                     <li className="ons-document-list__item-attribute">
-                                        <span className="ons-u-fw-b">State: </span>{item.state}
+                                        <span className="ons-u-fw-b" data-testid={`list-item-${index}-state`}>State: </span>{item.state}
                                     </li>
                                     : null
                                 }
@@ -28,7 +30,7 @@ export default function List({ items, noResultsText = "No results" }) {
                         </div>
                             { item.description ?
                                 <div className="ons-document-list__item-description">
-                                    <p>{ item.description }</p>
+                                    <p data-testid={`list-item-${index}-description`}>{ item.description }</p>
                                 </div>  : null
                             }
                     </div>
