62eade92f11bf9b51af4593b7e2a355446b768d1
jondewijones <jondewijones@gmail.com>
2025-06-19 13:33:14 +0100

    update mapper and add func cfor mapping state to user friendly string

--- src/app/series/[id]/editions/[editionID]/mapper.js
+++ src/app/series/[id]/editions/[editionID]/mapper.js
@@ -1,10 +1,25 @@
-export  function mapListItems(items, datasetID, editionID) {
+const mapStateToUserFriendlyString = (state) => {
+    switch (state) {
+        case "associated":
+            return "Draft";
+        case "approved":
+            return "Ready to publish";
+        case "published":
+            return "Published";
+        default:
+            return "Unable to get state";
+    }
+};
+
+const mapListItems = (items, datasetID, editionID) => {
     return items.map((item, index) => {
         return {
             id: index + 1,
             url: `/series/${datasetID}/editions/${editionID}/versions/${index + 1}`,
             title: `Version: ${index + 1}`,
-            published: item.state === "published" ? "Published" : "Not published yet"
+            state: mapStateToUserFriendlyString(item.state)
         };
     });
 };
+
+export { mapListItems, mapStateToUserFriendlyString };
