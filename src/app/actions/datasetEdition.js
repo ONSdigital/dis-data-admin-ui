f3619e2d1746c09d6b2b0c7f6a68ab4a3e8bf7a0
jondewijones <jondewijones@gmail.com>
2025-07-04 19:47:40 +0100

    add function to update editions and algin file structure with other actions

--- src/app/actions/datasetEdition.js
+++ src/app/actions/datasetEdition.js
@@ -2,12 +2,18 @@
 
 import { cookies } from "next/headers";
 
-import { httpPost, SSRequestConfig } from "@/utils/request/request";
+import { httpPost, httpPut, SSRequestConfig } from "@/utils/request/request";
 import { logInfo } from "@/utils/log/log";
+import { getFormData as getEditionWithVersionFormData } from "./datasetVersion";
 
 import { z } from "zod";
  
 const editionSchema = z.object({
+    edition: z.string().min(1, { message: "Edition ID is required" }),
+    edition_title: z.string().min(1, { message: "Edition title is required" })
+});
+
+const editionWithVersionSchema = z.object({
     edition: z.string().min(1, { message: "Edition ID is required" }),
     edition_title: z.string().min(1, { message: "Edition title is required" }),
     quality_designation: z.string().min(1, { message: "Quality designation is required" }),
@@ -21,94 +27,66 @@ const editionSchema = z.object({
     })).min(1, { message: "A file upload is required" })
 });
 
-// check for an error on distrubution and update error message
-// zod replies with "Required" for objects
-const addUploadFileErrorMessage = (errors) => {
-    if (!errors) {
-        return;
-    } 
-    if (errors.distributions) {
-        errors.distributions = ["File upload is required"];
-    }
-    return errors;
-};
-
-// check if any release date or time errors exists and merge them into one for time and date fieldset
-const mergeDateTimeErrors = (errors) => {
-    if (!errors) {
-        return;
-    } 
-    if (errors.release_day || errors.release_month || errors.release_year || errors.release_hour || errors.release_minutes) {
-        errors.release_date_time = ["A release time and date is required"];
-    }
-    return errors;
-};
-
-// check and parse "MultiContent" (e.g. alerts and usuage notes) fields 
-const parseMutliContentField = (multiItem) => {
-    if (!multiItem || !multiItem.length) return [];
-
-    const parsedItems = [];
-    multiItem.forEach(item => {
-        const parsed = JSON.parse(item);
-        if (parsed.type || parsed.title) {
-            parsedItems.push(parsed);
-        }
-    });
-    return parsedItems;
-};
-
-export async function createDatasetEdition(currentstate, formData) {
-    let actionResponse = {};
-
-    const datasetID = formData.get("dataset-id");
-    const usageNotes = formData.getAll("usage-notes");
-    const parsedUsageNotes = parseMutliContentField(usageNotes);
-    const alerts = formData.getAll("alerts");
-    const parsedAlerts = parseMutliContentField(alerts);
-    const datasetEdition = {
-        edition: formData.get("edition-id"),
-        edition_title: formData.get("edition-title"),
-        quality_designation: formData.get("quality-designation-value"),
-        release_day: formData.get("release-date-day"),
-        release_month: formData.get("release-date-month"),
-        release_year: formData.get("release-date-year"),
-        release_hour: formData.get("release-date-hour"),
-        release_minutes: formData.get("release-date-minutes"),
-        usage_notes: parsedUsageNotes,
-        alerts: parsedAlerts,
-        distributions: [ JSON.parse(formData.get('dataset-upload-value')) ],
-        release_date: null,
-        type: "static"
-    };
-
-    const validation = editionSchema.safeParse(datasetEdition);
-
-    if (!validation.success) {
-        actionResponse.success = validation.success;
-        actionResponse.errors = validation.error.flatten().fieldErrors;
-        actionResponse.errors = addUploadFileErrorMessage(actionResponse.errors);
-        actionResponse.errors = mergeDateTimeErrors(actionResponse.errors);
-        actionResponse.submission = datasetEdition;
-        logInfo("failed dataset edition validation", null, null);
-        return actionResponse;
-    } 
-
-    datasetEdition.release_date =  new Date(datasetEdition.release_year, datasetEdition.release_month - 1, datasetEdition.release_day, datasetEdition.release_hour, datasetEdition.release_minutes).toISOString();
-
+const doSubmission = async (datasetEditionSubmission, makeRequest) => {
+    const actionResponse = {};
     const reqCfg = await SSRequestConfig(cookies);
+
+    let url = `/datasets/${datasetEditionSubmission.dataset_id}/editions/${datasetEditionSubmission.edition_id}/versions`;
+    if (!datasetEditionSubmission.version_id) { url = url + `/1`; }
+
     try {
-        const data = await httpPost(reqCfg, `/datasets/${datasetID}/editions/${datasetEdition.edition}/versions`, datasetEdition);
+        const data = await makeRequest(reqCfg, url, datasetEditionSubmission);
         actionResponse.success = true;
         if (data.status >= 400) {
             actionResponse.success = false;
             actionResponse.code = data.status;
-            return;
-        } 
-        logInfo("created dataset edition successfully", null, null);
+            return actionResponse;
+        }
+        logInfo("saved dataset edition successfully", null, null);
     } catch (err) {
         return err.toString();
     }
-
     return actionResponse;
-}
+};
+
+const getFormData = (formData) => {
+    return {
+        dataset_id: formData.get("dataset-id"),
+        edition_id: formData.get("current-edition-id"),
+        edition: formData.get("edition-id"),
+        edition_title: formData.get("edition-title"),
+    };
+};
+
+const handleFailedValidation = (validation, datasetEditionSubmission) => {
+    const actionResponse = {};
+    actionResponse.success = validation.success;
+    actionResponse.errors = validation.error.flatten().fieldErrors;
+    actionResponse.errors = addUploadFileErrorMessage(actionResponse.errors);
+    actionResponse.errors = mergeDateTimeErrors(actionResponse.errors);
+    actionResponse.submission = datasetEditionSubmission;
+    logInfo("failed dataset edition validation", null, null);
+    return actionResponse;
+};
+
+const createDatasetEdition = async (currentstate, formData) => {
+    const datasetEditionSubmission = getEditionWithVersionFormData(formData);
+    const validation = editionWithVersionSchema.safeParse(datasetEditionSubmission);
+
+    if (!validation.success) {
+        return handleFailedValidation(validation, datasetEditionSubmission);
+    }
+    return doSubmission(datasetEditionSubmission, httpPost);
+};
+
+const updateDatasetEdition = async (currentstate, formData) => {
+    const datasetEditionSubmission = getFormData(formData);
+    const validation = editionSchema.safeParse(datasetEditionSubmission);
+
+    if (!validation.success) {
+        return handleFailedValidation(validation, datasetEditionSubmission);
+    }
+    return doSubmission(datasetEditionSubmission, httpPut);
+};
+
+export { createDatasetEdition, updateDatasetEdition };
