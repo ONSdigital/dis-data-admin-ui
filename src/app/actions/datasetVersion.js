dd9f6893184c50165644dc99d38828367a2151a4
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-07-15 08:27:21 +0100

    Version create test passing

--- src/app/actions/datasetVersion.js
+++ src/app/actions/datasetVersion.js
@@ -64,8 +64,9 @@ const doSubmission = async (datasetVersionSubmission, makeRequest) => {
     let url = `/datasets/${datasetVersionSubmission.dataset_id}/editions/${datasetVersionSubmission.edition}/versions`;
     if (datasetVersionSubmission.version_id) { url = url + `/${datasetVersionSubmission.version_id}`; }
 
+    let data = {}
     try {
-        const data = await makeRequest(reqCfg, url, datasetVersionSubmission);
+        data = await makeRequest(reqCfg, url, datasetVersionSubmission);
         actionResponse.success = true;
         if (data.status >= 400) {
             actionResponse.success = false;
@@ -77,7 +78,7 @@ const doSubmission = async (datasetVersionSubmission, makeRequest) => {
         return err.toString();
     }
     if (actionResponse.success == true) {
-        redirect("/series/" + datasetVersionSubmission.dataset_id + "/editions/" + datasetVersionSubmission.edition + "/versions/" + datasetVersionSubmission.version_id + "?display_success=true");
+        redirect("/series/" + datasetVersionSubmission.dataset_id + "/editions/" + datasetVersionSubmission.edition + "/versions/" + data.version + "?display_success=true");
     }
     return actionResponse;
 };
