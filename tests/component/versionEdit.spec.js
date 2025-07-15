87c08bde58a5ff62609b7351d60d7439673b43a7
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-07-15 06:59:45 +0100

    Create dataset series test and edit dataset version test passing

--- tests/component/versionEdit.spec.js
+++ tests/component/versionEdit.spec.js
@@ -49,6 +49,9 @@ test.describe("Create version page", () => {
 
         await page.getByRole("button", { name: /Save new dataset version/i }).click();
 
-        await expect(page.getByText("Dataset version saved successfully.")).toBeVisible();
+        await page.waitForURL("**/series/mock-quarterly/editions/time-series/versions/1**");
+        await expect(page.url().toString()).toContain("series/mock-quarterly/editions/time-series/versions/1");
+
+        await expect(page.getByText("Dataset version saved")).toBeVisible();
     });
 });
