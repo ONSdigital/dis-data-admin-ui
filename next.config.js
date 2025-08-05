c309474be436c882e2368e24453ad0fbf51cbbe6
jondewijones <jondewijones@gmail.com>
2025-08-05 20:10:24 +0100

    fix file formatting

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
-    output: "standalone",
-    basePath: "/data-admin",
-    experimental: {
-        serverActions: {
-          allowedOrigins: [
-            "publishing.dp.aws.onsdigital.uk", // sandbox
-            "publishing.dp-staging.aws.onsdigital.uk", // staging
-            "localhost:29500" // local when running in dataset-catalogue stack
-          ],
-        },
-      },
+  output: "standalone",
+  basePath: "/data-admin",
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "publishing.dp.aws.onsdigital.uk", // sandbox
+        "publishing.dp-staging.aws.onsdigital.uk", // staging
+        "localhost:29500" // local when running in dataset-catalogue stack
+      ],
+    },
+  },
 };
