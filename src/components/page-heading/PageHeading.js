bba95243b100649280a5f433029a3032132ed7af
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-08-05 11:46:39 +0100

    pageHeading tests

--- src/components/page-heading/PageHeading.js
+++ src/components/page-heading/PageHeading.js
@@ -13,13 +13,14 @@ export default function PageHeading(props){
 
     return (
         <>
-            <h1 className="ons-u-fs-m ons-u-mb-xxs" style={headingStyle}>{props.heading}</h1>
-            <h2 className="ons-u-fs-xl ons-u-mb-l">{props.title}</h2>
+            <h1 data-testid="page-heading-header" className="ons-u-fs-m ons-u-mb-xxs" style={headingStyle}>{props.heading}</h1>
+            <h2 data-testid="page-heading-title" className="ons-u-fs-xl ons-u-mb-l">{props.title}</h2>
                 <Button
+                    dataTestId="page-heading-create-button"
                     text={props.createMessage}
                     onClick={() => router.push(props.linkCreate)}                 
                 />     
-            <Link href={props.linkBack} className="ons-u-fs-s ons-u-dib ons-u-mt-s ons-u-ml-s">{props.backToMessage}</Link>
+            <Link data-testid="page-heading-back-link" href={props.linkBack} className="ons-u-fs-s ons-u-dib ons-u-mt-s ons-u-ml-s">{props.backToMessage}</Link>
         </>
   );
 }
