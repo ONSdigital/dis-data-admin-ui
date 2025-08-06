317088e2be598eeaa2ca14de2a5244aaeb6fe8bf
jondewijones <jondewijones@gmail.com>
2025-08-06 20:11:02 +0100

    fix file formatting

--- src/app/series/page.js
+++ src/app/series/page.js
@@ -5,14 +5,14 @@ import { httpGet, SSRequestConfig } from "@/utils/request/request";
 import Hero from "@/components/hero/Hero";
 import Panel from "@/components/panel/Panel";
 import List from "@/components/list/List";
-import Pagination  from "@/components/pagination/Pagination";
+import Pagination from "@/components/pagination/Pagination";
 import { mapListItems } from "./mapper";
 
-export default async function Series({searchParams}) {
+export default async function Series({ searchParams }) {
     const reqCfg = await SSRequestConfig(cookies);
 
     const pageParams = await searchParams;
-    if (Object.keys(pageParams).length === 0){
+    if (Object.keys(pageParams).length === 0) {
         pageParams.limit = 25;
     }
 
@@ -27,7 +27,7 @@ export default async function Series({searchParams}) {
         listItems.push(...mapListItems(data.items));
     }
 
-    const totalNumberOfPages = Math.ceil(data.total_count/pageParams.limit);
+    const totalNumberOfPages = Math.ceil(data.total_count / pageParams.limit);
     const currentPage = Math.floor(pageParams.offset ? (pageParams.offset / pageParams.limit) + 1 : 1);
 
     const renderErrorPanel = () => {
@@ -37,24 +37,24 @@ export default async function Series({searchParams}) {
             </Panel>
         )
     }
-    
+
     return (
         <>
-            {!datasetFetchError ? 
+            {!datasetFetchError ?
                 <>
-                <Hero hyperLink={{ text: "Add new dataset series", url: "series/create"}} title="Dataset series" wide/>
-                <div className="ons-u-mt-l ons-u-mb-l">
-                <List items={listItems} type="series"/>
-                <Pagination
-                    totalNumberOfPages = {totalNumberOfPages}
-                    currentPage = {currentPage}
-                    limit = {pageParams.limit}
-                />
-                </div>
+                    <Hero hyperLink={{ text: "Add new dataset series", url: "series/create" }} title="Dataset series" wide />
+                    <div className="ons-u-mt-l ons-u-mb-l">
+                        <List items={listItems} type="series" />
+                        <Pagination
+                            totalNumberOfPages={totalNumberOfPages}
+                            currentPage={currentPage}
+                            limit={pageParams.limit}
+                        />
+                    </div>
                 </>
                 : renderErrorPanel()
             }
-            
+
         </>
     );
 }
