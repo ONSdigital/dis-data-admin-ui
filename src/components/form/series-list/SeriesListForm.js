5cc315fca69ad09e504b99f3f753773ab4f9e8de
jondewijones <jondewijones@gmail.com>
2025-08-11 13:49:29 +0100

    add basic series filter form

---
+++ src/components/form/series-list/SeriesListForm.js
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { TextInput } from "author-design-system-react";
+
+export default function SeriesListForm({datasetID}) {
+    const [id, setID] = useState(datasetID || "");
+    const { push } = useRouter();
+    const handleSearchCLick = () => {
+        if (id.length > 0) {
+            push(`/series/?id=${id}`);
+        }
+    }
+    return (
+        <>    
+            <TextInput id="series-list-search-by-id" 
+                dataTestId="series-list-search-by-id" 
+                label={{text: "Search by ID", description: "E.g \"CPIH01\""}}
+                value={id}
+                onChange={e => setID(e.target.value)}
+                searchButton={{text: "Search", onClick: handleSearchCLick}} 
+            />
+        </>
+    );
+}
