73ca52395450b880a36b83ebb270f00f2e72e583
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-08-11 13:54:48 +0100

    Swapped around h tags and changed how color for h1 is done

--- src/components/page-heading/PageHeading.js
+++ src/components/page-heading/PageHeading.js
@@ -4,17 +4,13 @@ import Link from "next/link";
 import { Button } from "author-design-system-react";
 import { useRouter } from "next/navigation"
 
-const headingStyle = {
-    color: "#206095"
-};
- 
 export default function PageHeading(props){
     const router = useRouter()
 
     return (
         <>
-            <h1 data-testid="page-heading-header" className="ons-u-fs-m ons-u-mb-xxs" style={headingStyle}>{props.heading}</h1>
-            <h2 data-testid="page-heading-title" className="ons-u-fs-xl ons-u-mb-l">{props.title}</h2>
+            <h2 data-testid="page-heading-header" className="ons-u-fs-m ons-u-mb-xxs" style={{color: "var(--ons-color-branded-text)"}}>{props.heading}</h2>
+            <h1 data-testid="page-heading-title" className="ons-u-fs-xl ons-u-mb-l">{props.title}</h1>
                 <Button
                     dataTestId="page-heading-create-button"
                     text={props.createMessage}
