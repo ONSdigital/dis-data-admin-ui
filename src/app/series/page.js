93bc9ee072b94d90782b87d63bb4fdc13f43febf
jondewijones <jondewijones@gmail.com>
2025-08-11 14:24:21 +0100

    add search filter

--- src/app/series/page.js
+++ src/app/series/page.js
@@ -6,18 +6,17 @@ import Hero from "@/components/hero/Hero";
 import Panel from "@/components/panel/Panel";
 import List from "@/components/list/List";
 import Pagination from "@/components/pagination/Pagination";
+import SeriesListForm from "@/components/form/series-list/SeriesListForm";
 import { mapListItems } from "./mapper";
 
 export default async function Series({ searchParams }) {
     const reqCfg = await SSRequestConfig(cookies);
 
     const pageParams = await searchParams;
-    if (Object.keys(pageParams).length === 0) {
-        pageParams.limit = 25;
-    }
+    pageParams.limit = 20;
 
-    const url = "/datasets?type=static&sort_order=ASC&limit=" + pageParams.limit + (pageParams.offset ? "&offset=" + pageParams.offset : "");
-    const data = await httpGet(reqCfg, url);
+    const requestURL = generateRequestURL(pageParams);
+    const data = await httpGet(reqCfg, requestURL);
 
     let datasetFetchError = false;
     const listItems = [];
@@ -43,13 +42,18 @@ export default async function Series({ searchParams }) {
             {!datasetFetchError ?
                 <>
                     <Hero hyperLink={{ text: "Add new dataset series", url: "series/create" }} title="Dataset series" wide />
-                    <div className="ons-u-mt-l ons-u-mb-l">
-                        <List items={listItems} type="series" />
-                        <Pagination
-                            totalNumberOfPages={totalNumberOfPages}
-                            currentPage={currentPage}
-                            limit={pageParams.limit}
-                        />
+                    <div className="ons-grid ons-u-mt-l ons-u-mb-l">
+                        <div className="ons-grid__col ons-col-4@m ons-grid--gutterless">
+                            <SeriesListForm />
+                        </div>
+                        <div className="ons-grid__col ons-col-8@m">
+                            <List items={listItems} type="series" />
+                            <Pagination
+                                totalNumberOfPages={totalNumberOfPages}
+                                currentPage={currentPage}
+                                limit={pageParams.limit}
+                            />
+                        </div>
                     </div>
                 </>
                 : renderErrorPanel()
@@ -57,4 +61,16 @@ export default async function Series({ searchParams }) {
 
         </>
     );
+}
+
+const generateRequestURL = (params) => {
+    let url = `/datasets?type=static&sort_order=ASC&limit=${params.limit}`;
+    if (params.id && params.id.length > 0) {
+        url = `${url}&id=${params.id}`;
+    }
+
+    if (params.offset) {
+        url = `${url}&offset=${params.offset}`;
+    }
+    return url;
 }
