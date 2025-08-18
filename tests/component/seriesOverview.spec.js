2906ac688d5e821b8e6e338aaf1025b3040e31dc
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-08-18 11:26:45 +0100

    Test refactor

--- tests/component/seriesOverview.spec.js
+++ tests/component/seriesOverview.spec.js
@@ -8,7 +8,7 @@ test.describe("Series overview page", () => {
 
         await page.goto("./series/mock-quarterly");
         await expect(page.getByTestId("page-heading-header")).toContainText("Series");
-        await expect(page.getByRole("link", { name: "time-series" })).toBeVisible();
+        await expect(page.getByRole("link", { name: "Timeseries" })).toBeVisible();
         await expect(page.getByTestId("id-field")).toContainText("mock-quarterly");
         await expect(page.getByTestId("type-field")).toContainText("static");
         await expect(page.getByTestId("title-field")).toContainText("Mock Dataset");
