abce2057c12e9a110f3629c750885af8ebe6b8e6
jondewijones <jondewijones@gmail.com>
2025-09-01 14:37:57 +0100

    uncomment commented tests (commented for debugging)

--- tests/component/seriesEdit.spec.js
+++ tests/component/seriesEdit.spec.js
@@ -14,33 +14,33 @@ test.describe("Edit series page", () => {
         await expect(page.url().toString()).toContain("series/mock-quarterly/edit#dataset-series-title");
     });
 
-    // test("Submit form successfully", async ({ page, context }) => {
-    //     addValidAuthCookies(context);
+    test("Submit form successfully", async ({ page, context }) => {
+        addValidAuthCookies(context);
 
-    //     await page.goto("./series/mock-quarterly/edit")
+        await page.goto("./series/mock-quarterly/edit")
 
-    //     await page.getByTestId("dataset-series-title").fill("test edit title");
-    //     await page.getByLabel("Topics").selectOption("1001");
-    //     await page.getByRole("button", { name: /Add Topic/i }).click();
-    //     await page.getByTestId("field-dataset-series-description").getByRole("textbox").fill("test edit description");
-    //     await page.getByTestId("dataset-series-qmi").fill("test-url.com");
-    //     await page.getByTestId("dataset-series-keywords").fill("test,keywords,foo,bar");
-    //     await page.getByLabel("Name").fill("test edit name");
-    //     await page.getByLabel("Email").fill("test-email-edit@test.com");
-    //     await page.getByRole("button", { name: /Add contact/i }).click();
-    //     await page.getByRole("button", { name: /Save new dataset series/i }).click();
+        await page.getByTestId("dataset-series-title").fill("test edit title");
+        await page.getByLabel("Topics").selectOption("1001");
+        await page.getByRole("button", { name: /Add Topic/i }).click();
+        await page.getByTestId("field-dataset-series-description").getByRole("textbox").fill("test edit description");
+        await page.getByTestId("dataset-series-qmi").fill("test-url.com");
+        await page.getByTestId("dataset-series-keywords").fill("test,keywords,foo,bar");
+        await page.getByLabel("Name").fill("test edit name");
+        await page.getByLabel("Email").fill("test-email-edit@test.com");
+        await page.getByRole("button", { name: /Add contact/i }).click();
+        await page.getByRole("button", { name: /Save new dataset series/i }).click();
 
-    //     await expect(page.getByText("Dataset series saved")).toBeVisible();
-    // });
+        await expect(page.getByText("Dataset series saved")).toBeVisible();
+    });
 
-    // test("Does not allow duplicate dataset series title to be created", async ({ page, context }) => {
-    //     addValidAuthCookies(context);
+    test("Does not allow duplicate dataset series title to be created", async ({ page, context }) => {
+        addValidAuthCookies(context);
 
-    //     await page.goto("./series/mock-quarterly/edit")
+        await page.goto("./series/mock-quarterly/edit")
 
-    //     await page.getByTestId("dataset-series-title").fill("duplicate-title");
-    //     await page.getByRole("button", { name: /Save new dataset series/i }).click();
+        await page.getByTestId("dataset-series-title").fill("duplicate-title");
+        await page.getByRole("button", { name: /Save new dataset series/i }).click();
 
-    //     await expect(page.getByText("A dataset series titled duplicate-title already exists")).toBeVisible();
-    // });
+        await expect(page.getByText("A dataset series titled duplicate-title already exists")).toBeVisible();
+    });
 });
