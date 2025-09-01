44a8c58ab33e43a78322ee1486ad529675a965a1
jondewijones <jondewijones@gmail.com>
2025-09-01 19:30:05 +0100

    remove hardcoded values and debug logging

--- src/utils/breadcrumb/breadcrumb.js
+++ src/utils/breadcrumb/breadcrumb.js
@@ -1,37 +1,14 @@
-[
-  {
-    "url": "#0",
-    "text": "Home",
-    "dataTestId": "home-link"
-  },
-  {
-    "url": "#0",
-    "text": "Components",
-    "dataTestId": "components-link"
-  }
-]
-
 const baseURL = "/data-admin/series";
 
 const firstBreadcrumb = {
     url: baseURL,
     text: "Dataset catalogue",
     dataTestId: "breadcrumb-catalogue"
-}
+};
 
 const generateBreadcrumb = (currentURL, datasetTitle, editionTitle) => {
     const urlSplit = currentURL.split("/")
     const breadcrumbs = [ firstBreadcrumb ]
-
-    console.log("0:", urlSplit[0])
-    console.log("1:", urlSplit[1])
-    console.log("2:", urlSplit[2])
-    console.log("3:", urlSplit[3])
-    console.log("4:", urlSplit[4])
-    console.log("5:", urlSplit[5])
-    console.log("6:", urlSplit[6])
-    console.log("7:", urlSplit[7])
-    
 
     // add dataset series
     if (urlSplit.length >= 3) {
