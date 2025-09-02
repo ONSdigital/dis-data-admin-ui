dd2861feeda75eb33bdb91eb446325fa7152b7f5
jondewijones <jondewijones@gmail.com>
2025-09-02 16:27:15 +0100

    update to include breadcrumb, panel and ability to disable button

--- src/components/page-heading/PageHeading.js
+++ src/components/page-heading/PageHeading.js
@@ -1,23 +1,65 @@
 "use client";
 
 import Link from "next/link";
-import { Button, Breadcrumb } from "author-design-system-react";
+import { Button, Breadcrumb, Panel } from "author-design-system-react";
 import { useRouter } from "next/navigation";
 
 export default function PageHeading(props){
     const router = useRouter();
 
-    return (
-        <>
-            <Breadcrumb itemsList={props.breadcrumbs}/>
-            <h2 data-testid="page-heading-header" className="ons-u-fs-m ons-u-mb-xxs" style={{color: "var(--ons-color-branded-text)"}}>{props.heading}</h2>
-            <h1 data-testid="page-heading-title" className="ons-u-fs-xl ons-u-mb-l">{props.title}</h1>
+    const renderBreadcrumb = () => {
+        if (props.breadcrumbs) {
+            return (
+                <Breadcrumb itemsList={props.breadcrumbs} classes="ons-u-mb-xs"/>
+            )
+        }
+    };
+
+    const renderSubtitle = () => {
+        if (props.subtitle) {
+            return (
+                <h2 data-testid="page-heading-subtitle" className="ons-u-fs-m ons-u-mb-xxs" style={{color: "var(--ons-color-branded-text)"}}>{props.subtitle}</h2>
+            )
+        }
+    };
+
+    const renderButton = () => {
+        if (props.buttonText && props.buttonURL) {
+            return (
                 <Button
                     dataTestId="page-heading-create-button"
-                    text={props.createMessage}
-                    onClick={() => router.push(props.linkCreate)}                 
-                />     
-            <Link data-testid="page-heading-back-link" href={props.linkBack} className="ons-u-fs-s ons-u-dib ons-u-mt-s ons-u-ml-s">{props.backToMessage}</Link>
+                    text={props.buttonText}
+                    onClick={() => router.push(props.buttonURL)}      
+                    variants={props.disableButton ? "disabled" : "primary"}         
+                /> 
+            )
+        }
+    };
+
+    const renderPanel = () => {
+        if (props.showPanel && props.panelText) {
+            return (
+                <Panel variant="info" classes="ons-u-mb-l" dataTestId="page-heading-panel"><p>{props.panelText}</p></Panel>
+            )
+        }
+    }
+
+    const renderLink = () => {
+        if (props.linkText && props.linkURL) {
+            return (
+                <Link data-testid="page-heading-back-link" href={props.linkURL} className="ons-u-fs-s ons-u-dib ons-u-mt-s ons-u-ml-s">{props.linkText}</Link>
+            )
+        }
+    };
+
+    return (
+        <>
+            { renderBreadcrumb() }
+            { renderSubtitle() }
+            <h1 data-testid="page-heading-title" className="ons-u-fs-xl ons-u-mb-m">{props.title}</h1>
+            { renderPanel() }
+            { renderButton() }
+            { renderLink() }
         </>
   );
 }
