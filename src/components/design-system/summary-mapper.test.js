5546f0ab08a866b4fa46f01f4f8702e08f59c15e
jondewijones <jondewijones@gmail.com>
2025-09-18 21:42:38 +0100

    add tests for edition summary mapper

--- src/components/design-system/summary-mapper.test.js
+++ src/components/design-system/summary-mapper.test.js
@@ -1,5 +1,6 @@
-import { mapSeriesSummary } from "./summary-mapper";
+import { mapSeriesSummary, mapEditionSummary } from "./summary-mapper";
 import { datasetList } from "../../../tests/mocks/datasets.mjs";
+import { versions } from "../../../tests/mocks/versions.mjs";
 
 describe("mapSeriesSummary", () => {
     test("returns expected object of mapped content items", () => {
@@ -91,3 +92,28 @@ describe("mapSeriesSummary", () => {
     });
 });
 
+test("mapEditionSummary returns expected object of mapped content items", () => {
+    const mapped = mapEditionSummary(versions.items[0], "test/foo/edit", ["Topic Foo", "Topic Bar"]);
+    const mappedItems = mapped[0].groups[0].rows;
+    expect(mappedItems).toHaveLength(3);
+    // expect "Edition ID" to have single value and have "edit" action
+    expect(mappedItems[0].rowTitle).toBe("Edition ID");
+    expect(mappedItems[0].rowItems[0].valueList[0]).toMatchObject({text: "time-series"});
+    expect(mappedItems[0].rowItems[0].actions[0]).toMatchObject({
+        id: "edit-edition-id",
+        visuallyHiddenText: "Edit Edition ID",
+        url: "test/foo/edit#edition-id"
+    });
+    // expect "Edition title" to have single value and have "edit" action
+    expect(mappedItems[1].rowTitle).toBe("Edition title");
+    expect(mappedItems[1].rowItems[0].valueList[0]).toMatchObject({text: "This is an edition title for version 1"});
+    expect(mappedItems[1].rowItems[0].actions[0]).toMatchObject({
+        id: "edit-edition-title",
+        visuallyHiddenText: "Edit Edition title",
+        url: "test/foo/edit#edition-title"
+    });
+    // expect "Release date" to have single value and not have "edit" action
+    expect(mappedItems[2].rowTitle).toBe("Release date");
+    expect(mappedItems[2].rowItems[0].valueList[0]).toMatchObject({text: "26 January 2025"});
+    expect(mappedItems[2].rowItems[0].actions).toBeFalsy();
+});
