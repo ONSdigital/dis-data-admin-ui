cb974af2770acb695eb54fd543ebaa9982c39bbe
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-09-19 11:48:43 +0100

    unit tests

--- src/components/form/series/seriesForm.test.js
+++ src/components/form/series/seriesForm.test.js
@@ -31,7 +31,7 @@ describe("Series Form", () => {
 
         expect(screen.getByTestId("dataset-series-id")).toBeInTheDocument();
         expect(screen.getByTestId("dataset-series-title")).toBeInTheDocument();
-        expect(screen.getByTestId("dataset-series-topics-checkbox")).toBeInTheDocument();
+        expect(screen.getByTestId("fieldset-dataset-series-topics-checkbox")).toBeInTheDocument();
         expect(screen.getByTestId("dataset-series-description")).toBeInTheDocument();
         expect(screen.getByTestId("dataset-series-qmi")).toBeInTheDocument();
         expect(screen.getByTestId("dataset-series-keywords")).toBeInTheDocument();
