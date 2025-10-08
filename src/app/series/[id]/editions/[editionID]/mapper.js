8df056da2ec3e16492d428e32a48ffdae86e1e79
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-10-08 10:25:23 +0100

    Refactored mapper to use item.version rather than derive it using the index

--- src/app/series/[id]/editions/[editionID]/mapper.js
+++ src/app/series/[id]/editions/[editionID]/mapper.js
@@ -17,8 +17,8 @@ const mapListItems = (items, datasetID, editionID) => {
     return items.map((item, index) => {
         return {
             id: index + 1,
-            url: `/series/${datasetID}/editions/${editionID}/versions/${index + 1}`,
-            title: `Version: ${index + 1}`,
+            url: `/series/${datasetID}/editions/${editionID}/versions/${item.version}`,
+            title: `Version: ${item.version}`,
             state: mapStateToUserFriendlyString(item.state), 
             release_date: formatDate(item.release_date),
         };
