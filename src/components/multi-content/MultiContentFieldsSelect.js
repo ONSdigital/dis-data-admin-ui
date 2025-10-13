910384a91efbe956294ff3d9cf07c9cdecf5b50f
jondewijones <jondewijones@gmail.com>
2025-10-13 14:56:41 +0100

    switch to using radio instead of select

--- src/components/multi-content/MultiContentFieldsSelect.js
+++ src/components/multi-content/MultiContentFieldsSelect.js
@@ -2,18 +2,18 @@
 
 import { useState, useEffect, useRef } from "react";
 
-import { Select } from "author-design-system-react";
+import { Select, Radios } from "author-design-system-react";
 import TextArea from "../textarea/Textarea";
 
 export default function MultiContentFieldsSelect({ id, index, field, onFieldsHaveContent }) {
     const [contentType, setContentType] = useState(field?.type || "");
     const [contentBody, setContentBody] = useState(field?.note || field?.description || "");
 
-    const selectID = id + "-select-" + index;
+    const radiosID = id + "-radios-" + index;
     const textareaID = id + "-textarea-" + index;
 
     const fieldsHaveContent = contentType.length > 0 && contentBody.length > 0;
-    
+
     // Store latest onFieldsHaveContent in a ref so we can call it from effects without
     // adding it to the dependency array (which could trigger an update loop)
     const onFieldsHaveContentRef = useRef(onFieldsHaveContent);
@@ -29,34 +29,50 @@ export default function MultiContentFieldsSelect({ id, index, field, onFieldsHav
         setState(value);
     };
 
+    const isChecked = (fieldValue) => {
+        if (fieldValue === contentType) { return true; }
+        return false;
+    }
+
     return (
         <div className="ons-u-mb-m">
-            <input id={id} name={id} data-testid={id} type="hidden" value={JSON.stringify({type: contentType, description: contentBody})} />
-            <Select id={selectID} dataTestId={selectID} name={selectID} label={{text: "Type"}} onChange={e => handleInputChange(setContentType, e)} value={contentType}
-                options={[
-                    {
-                        value:"",
-                        text: "Select an option",
-                        disabled: true
-                    },
-                    {
-                        value:"correction",
-                        text: "Correction",
-                    },
-                    {
-                        value:"alert",
-                        text: "Notice",
-                    }
-                ]}
+            <input id={id} name={id} data-testid={id} type="hidden" value={JSON.stringify({ type: contentType, description: contentBody })} />
+            <Radios
+                dataTestId={radiosID}
+                legend="Alert type"
+                name={radiosID}
+                borderless={true}
+                radios={{
+                    radioList: [
+                        {
+                            id: `${radiosID}-correction`,
+                            label: {
+                                text: "Correction"
+                            },
+                            value: "correction",
+                            onChange: e => setContentType(e.target.value),
+                            checked: isChecked("correction")
+                        },
+                        {
+                            id: `${radiosID}-notice`,
+                            label: {
+                                text: "Notice"
+                            },
+                            value: "alert",
+                            onChange: e => setContentType(e.target.value),
+                            checked: isChecked("alert")
+                        }
+                    ]
+                }}
             />
 
-            <TextArea 
+            <TextArea
                 id={textareaID}
                 dataTestId={textareaID}
                 name={textareaID}
-                label={{text: "Description"}}  
-                value={contentBody} 
-                key={textareaID} 
+                label={{ text: "Description" }}
+                value={contentBody}
+                key={textareaID}
                 onChange={e => handleInputChange(setContentBody, e.target.value)}
             />
         </div>
