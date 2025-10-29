ac7e17d1431d30004bb8385affa286630e2cf1d3
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-10-29 11:43:13 +0000

    Impletmented requested UX changes

--- src/components/form/version/VersionFields.js
+++ src/components/form/version/VersionFields.js
@@ -80,11 +80,11 @@ export default function VersionFields(props) {
                     ]
                 }}
             />
-            <h3 className="ons-u-mt-xl">Usage notes (optional)</h3>
-            <MultiContentItems id="usage-notes" fieldType="input" buttonLabel="Add new usage note" contentItems={props.fieldValues?.usage_notes || []}></MultiContentItems>
+            <h3 className="ons-u-mt-xl">Usage notes</h3>
+            <MultiContentItems id="usage-notes" fieldType="input" buttonLabel="Add additional usage note" contentItems={props.fieldValues?.usage_notes || []}></MultiContentItems>
 
-            <h3 className="ons-u-mt-xl">Alerts (optional)</h3>
-            <MultiContentItems id="alerts" fieldType="radios" buttonLabel="Add new alert" contentItems={props.fieldValues?.alerts || []}></MultiContentItems>
+            <h3 className="ons-u-mt-xl">Alerts</h3>
+            <MultiContentItems id="alerts" fieldType="radios" buttonLabel="Add additional alert" contentItems={props.fieldValues?.alerts || []}></MultiContentItems>
 
             <h2 className="ons-u-mt-xl">Dataset file</h2>
             <ResumableFileUpload id="dataset-upload"
