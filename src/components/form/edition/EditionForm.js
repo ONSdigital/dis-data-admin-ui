f5a5c16b99e76243c412e596909c559c22fea734
JoshSinghONS <joshua.singh@ons.gov.uk>
2025-11-04 15:35:35 +0000

    Changed text depending on if edition is new

--- src/components/form/edition/EditionForm.js
+++ src/components/form/edition/EditionForm.js
@@ -79,7 +79,11 @@ export default function EditionForm({ datasetID, edition, isNewEdition, showEdit
                 { isNewEdition ? <VersionFields errors={formState.errors} /> : null }
 
                 <button type="submit" className={isPending == true ? "ons-btn ons-btn ons-u-mt-l ons-btn--disabled" : "ons-btn ons-u-mt-l"} disabled={isPending} data-testid="edition-save-button">
-                    <span className="ons-btn__inner"><span className="ons-btn__text">Create edition</span></span>
+                    <span className="ons-btn__inner">
+                        <span className="ons-btn__text">
+                            { isNewEdition ? "Create Edition" : "Save edition" }
+                        </span>
+                    </span>
                 </button>
                 <LinkButton
                     dataTestId="edition-cancel-button"
