6b67cb45e134ea6551cce4e64e4c4f014d4bfd3a
jondewijones <jondewijones@gmail.com>
2025-11-12 21:33:29 +0000

    call table mapper for data

--- src/app/migration/page.js
+++ src/app/migration/page.js
@@ -3,16 +3,17 @@ import LinkButton from "@/components/link-button/LinkButton";
 import Table from "@/components/table/Table";
 
 import { migrationJobsList } from "../../../tests/mocks/migration-jobs.mjs";
+import { mapMigrationListTable } from "@/components/table/mapper";
 
 export default function MigrationList() {
-    let data, totalCount;
+    const mappedTable = mapMigrationListTable(migrationJobsList.items)
     const renderListArea = () => {
         return (
             <>
                 <div className="ons-u-bb">
                     <div className="ons-grid ons-u-mb-m">
                         <div className="ons-grid__col ons-col-8@m ons-u-fs-m ons-u-mt-s">
-                            Showing {data?.offset + 1} to {data?.offset + data?.count} of {totalCount} jobs
+                            Showing 1 to 5 of 5 jobs
                         </div>
                         <div className="ons-grid__col ons-col-2@m ons-push-1@m">
                             <LinkButton
@@ -22,7 +23,7 @@ export default function MigrationList() {
                         </div>
                     </div>
                 </div>
-                <Table />
+                <Table contents={mappedTable} classes="ons-u-mt-m" />
             </>
         );
     };
