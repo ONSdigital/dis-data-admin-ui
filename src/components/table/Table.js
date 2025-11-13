be30883a1e98e6fc65c630141cad68b6b74e7b13
jondewijones <jondewijones@gmail.com>
2025-11-13 21:30:11 +0000

    add data-testid attributes

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,13 @@
 "use client";
 
 import { useState } from "react";
+import { sanitiseString } from "author-design-system-react";
 
-export default function Table({ contents, caption, classes  }) {
+export default function Table({ contents, caption, classes, dataTestId  }) {
     const [rows, setRows] = useState(contents?.body?.rows || []);
     const [sorted, setSorted] = useState();
+
+    const sanitisedDataTestId = sanitiseString(dataTestId);
 
     const handleSort = (rowIndex) => {
         const isSameColumn = sorted?.rowIndex === rowIndex;
@@ -34,10 +37,11 @@ export default function Table({ contents, caption, classes  }) {
             <thead className="ons-table__head">
                 <tr className="ons-table__row">
                     {contents?.headers.map((header, index) => {
+                        const sanitisedHeaderLabel = sanitiseString(header.label);
                         return (
-                            <th scope="col" className="ons-table__header" aria-sort="none" key={header.label + index}>
+                            <th scope="col" className="ons-table__header" aria-sort="none" key={header.label + index} data-testid={`${sanitisedDataTestId}-header-${sanitisedHeaderLabel}`}>
                                 {header.isSortable ?
-                                    <button aria-label="Sort by Legal basis" type="button" data-index="3" className="ons-table__sort-button" onClick={() => {handleSort(index)}}>
+                                    <button aria-label="Sort by Legal basis" type="button" data-testid={`${sanitisedDataTestId}-sort-button-${sanitisedHeaderLabel}`} className="ons-table__sort-button" onClick={() => {handleSort(index)}}>
                                         {header.label}
                                         <svg id="sort-sprite-legal-basis-0" className="ons-icon" viewBox="0 0 12 19" xmlns="http://www.w3.org/2000/svg" focusable="false" fill="currentColor" role="img" aria-hidden="true">
                                             <path className="ons-topTriangle" d="M6 0l6 7.2H0L6 0zm0 18.6l6-7.2H0l6 7.2zm0 3.6l6 7.2H0l6-7.2z"></path>
@@ -45,7 +49,7 @@ export default function Table({ contents, caption, classes  }) {
                                         </svg>
                                     </button>
                                     :
-                                    <span className="ons-table__header-text">{header.label}</span>
+                                    <span data-testid={`${sanitisedDataTestId}-header-label-${sanitisedHeaderLabel}`} className="ons-table__header-text">{header.label}</span>
                                 }
                             </th>
                         )
@@ -58,7 +62,7 @@ export default function Table({ contents, caption, classes  }) {
     const renderTableBody = () => {
         if (!rows.length) {
             return (
-                <tbody className="ons-table__body"><tr><td>No data available</td></tr></tbody>
+                <tbody className="ons-table__body"><tr><td data-testid={`${sanitisedDataTestId}-no-data`}>No data available</td></tr></tbody>
             )
         }
         return (
@@ -68,7 +72,7 @@ export default function Table({ contents, caption, classes  }) {
                         <tr className="ons-table__row" key={`row-${index}`}>
                             {row.columns.map((cell, i) => {
                                 return (
-                                    <td className="ons-table__cell" key={`row-${index}cell-${i}`}>{cell.content}</td>
+                                    <td className="ons-table__cell" key={`row-${index}cell-${i}`} data-testid={`${sanitisedDataTestId}-cell-${index}-${i}`}>{cell.content}</td>
                                 )
                             })}
                         </tr>
@@ -81,8 +85,8 @@ export default function Table({ contents, caption, classes  }) {
     return (
         <div className={"ons-table-scrollable ons-table-scrollable--on " + classes}>
             <div className="ons-table-scrollable__content" tabIndex="0" role="region" aria-label="JavaScript enhanced sortable table. Scrollable table">
-                <table className="ons-table ons-table--sortable" data-aria-sort="Sort by" data-aria-asc="ascending" data-aria-desc="descending">
-                    { caption && <caption className="ons-table__caption"> {caption} </caption>}
+                <table data-testid={dataTestId} className="ons-table ons-table--sortable" data-aria-sort="Sort by" data-aria-asc="ascending" data-aria-desc="descending">
+                    { caption && <caption data-testid={`${sanitisedDataTestId}-caption`} className="ons-table__caption"> {caption} </caption>}
                     { renderTableHeader() }
                     { renderTableBody() }
                 </table>
