4cabc5b0d3be860baee8381e1fe01719a491b15e
jondewijones <jondewijones@gmail.com>
2025-12-02 13:34:19 +0000

    add unhappy path tests

--- tests/component/versionOverview.spec.js
+++ tests/component/versionOverview.spec.js
@@ -102,4 +102,18 @@ test.describe("version overview page", () => {
         await page.goto("./series/mock-quarterly/editions/time-series/versions/1");
         await expect(page.getByTestId("delete-version-button")).not.toBeVisible();
     });
+
+    test.describe("handles API error", () => {
+        test("when 404 is returned", async ({ page, context }) => {
+            addValidAuthCookies(context);
+            await page.goto("./series/404");
+            expect(page.getByText("There was an issue retrieving the list of dataset series. Refresh the page to try again."));
+        });
+
+         test("when 500 is returned", async ({ page, context }) => {
+            addValidAuthCookies(context);
+            await page.goto("./series/500");
+            expect(page.getByText("There was an issue retrieving the list of dataset series. Refresh the page to try again."));
+        });
+    });
 });
