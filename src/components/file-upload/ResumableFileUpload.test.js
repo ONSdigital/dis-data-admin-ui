1e7def688a951eafaa29403a9f796368f4258ab8
jondewijones <jondewijones@gmail.com>
2025-12-05 10:03:31 +0000

    fix formatting

--- src/components/file-upload/ResumableFileUpload.test.js
+++ src/components/file-upload/ResumableFileUpload.test.js
@@ -1,9 +1,8 @@
 import "@testing-library/jest-dom"
 import { render, screen, fireEvent } from "@testing-library/react"
 
-
-jest.mock('uuid', () => ({
-  v4: () => '12345678-1234-1234-1234-123456789012',
+jest.mock("uuid", () => ({
+    v4: () => "12345678-1234-1234-1234-123456789012",
 }));
 
 import ResumableFileUpload from "./ResumableFileUpload";
