ec0a78dc6ebe20a1521267bdb67fb97298de1c1d
jondewijones <jondewijones@gmail.com>
2026-01-13 20:06:28 +0000

    add hidden dataset id field

--- src/components/form/publish/PublishForm.js
+++ src/components/form/publish/PublishForm.js
@@ -4,7 +4,7 @@ import { useActionState } from "react";
 
 import Link from "next/link";
 
-export default function PublishForm({ action, datasetTitle, cancelLink}) {
+export default function PublishForm({ action, datasetID, datasetTitle, cancelLink}) {
     const [formState, formAction, isPending] = useActionState(action, {});
 
     if(isPending){
@@ -14,7 +14,8 @@ export default function PublishForm({ action, datasetTitle, cancelLink}) {
     return (
         <form action={formAction}>
             <h1 className="ons-u-fs-xl ons-u-mb-no">{`Are you sure you want to publish "${datasetTitle}"?`}</h1>
-            <p className="ons-u-mb-l">Approving this action will make the dataset visible to the public.</p>
+            <p className="ons-u-mb-l">Approving this action will make the dataset series visible to the public.</p>
+            <input type="hidden" name="dataset-id" value={datasetID} data-testid="hidden-dataset-id" readOnly />
             <button data-testid="dataset-series-publish-button" type="submit" className={`ons-btn ons-u-mr-m ${isPending === true &&  "ons-btn--disabled"}`} disabled={isPending}>
                 <span className="ons-btn__inner jon">
                     <span className="ons-btn__text">
