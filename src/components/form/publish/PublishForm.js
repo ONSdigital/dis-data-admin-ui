f24beb37ce9627b7c900d5eeb113ea6aff0a0d01
jondewijones <jondewijones@gmail.com>
2026-01-13 19:37:43 +0000

    move publish form to client component

---
+++ src/components/form/publish/PublishForm.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { useActionState } from "react";
+
+import Link from "next/link";
+
+export default function PublishForm({ action, datasetTitle, cancelLink}) {
+    const [formState, formAction, isPending] = useActionState(action, {});
+
+    if(isPending){
+        window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    }
+
+    return (
+        <form action={formAction}>
+            <h1 className="ons-u-fs-xl ons-u-mb-no">{`Are you sure you want to publish "${datasetTitle}"?`}</h1>
+            <p className="ons-u-mb-l">Approving this action will make the dataset visible to the public.</p>
+            <button data-testid="dataset-series-publish-button" type="submit" className={`ons-btn ons-u-mr-m ${isPending === true &&  "ons-btn--disabled"}`} disabled={isPending}>
+                <span className="ons-btn__inner jon">
+                    <span className="ons-btn__text">
+                        Publish
+                    </span>
+                </span>
+            </button>
+            <Link href={cancelLink} className="ons-u-dib ons-u-mt-xs" data-testid="dataset-series-cancel-link">Cancel</Link>
+        </form>
+    );
+}
