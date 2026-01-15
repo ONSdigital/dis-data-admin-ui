3b1ddb9c71788b527894cbfee40ce833713afa18
jondewijones <jondewijones@gmail.com>
2026-01-15 19:03:44 +0000

    add tests for form

---
+++ src/components/form/publish/PublishForm.test.js
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import PublishForm from "./PublishForm";
+import { datasetList } from "../../../../tests/mocks/datasets.mjs";
+
+jest.mock("next/navigation", () => ({ 
+    useRouter: jest.fn().mockReturnValue({ 
+        push: jest.fn(), 
+    }), 
+}));
+
+describe("Publish form", () => {
+    test("renders correctly", () => {
+        render(<PublishForm dataset={ datasetList.items[0] } cancelLink={"./"}  />);
+
+        expect(screen.getByText(/Are you sure you want to publish "Consumer prices"?/i)).toBeInTheDocument();
+        expect(screen.getByText(/Approving this action will make the dataset series visible to the public./i)).toBeInTheDocument();
+        expect(screen.getByTestId("hidden-dataset")).toBeInTheDocument();
+        expect(screen.getByTestId("dataset-series-publish-button")).toBeInTheDocument();
+        expect(screen.getByTestId("dataset-series-cancel-link")).toBeInTheDocument();
+    });
+});
