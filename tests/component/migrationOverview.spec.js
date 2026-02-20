a8174cc1aaa1c5cc2e4d4fdaf0b126b7ac8386f1
jondewijones <jondewijones@gmail.com>
2026-02-20 13:19:24 +0000

    add unhappy path tests

--- tests/component/migrationOverview.spec.js
+++ tests/component/migrationOverview.spec.js
@@ -46,4 +46,18 @@ test.describe("Migration overview page", () => {
         await expect(page.locator("#version-4")).toContainText("4");
         await expect(page.locator("#version-5")).toContainText("5");
     });
+
+    test.describe("Handles API error", () => {
+        test("When 404 is returned", async ({ page, context }) => {
+            setValidAuthCookies(context);
+            await page.goto("./migration/404");
+            await expect(page.getByText("There was an issue retrieving the data for this page. Try refreshing the page.")).toBeVisible();
+        });
+
+            test("When 500 is returned", async ({ page, context }) => {
+            setValidAuthCookies(context);
+            await page.goto("./migration/500");
+            await expect(page.getByText("There was an issue retrieving the data for this page. Try refreshing the page.")).toBeVisible();
+        });
+    });
 });
