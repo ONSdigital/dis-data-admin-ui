460a8d7a765f9ca7884c2f7ff0c1afee76355a31
jondewijones <jondewijones@gmail.com>
2026-03-11 20:20:51 +0000

    import panel component

--- src/app/migration/create/page.js
+++ src/app/migration/create/page.js
@@ -1,4 +1,5 @@
 import PageHeading from "@/components/page-heading/PageHeading";
+import { Panel } from "@/components/design-system/DesignSystem";
 import MigrationForm from "@/components/form/migration/MigrationForm";
 import { createMigrationJob } from "@/app/actions/createMigrationJob";
 
