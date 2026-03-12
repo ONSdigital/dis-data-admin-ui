0f19172f4232b9c933a350028a70ad57e1790a6c
jondewijones <jondewijones@gmail.com>
2026-03-12 13:44:27 +0000

    use count instead of total_count

--- src/app/migration/page.js
+++ src/app/migration/page.js
@@ -31,7 +31,7 @@ export default async function MigrationList() {
                 <div className="ons-u-bb">
                     <div className="ons-grid ons-u-mb-m">
                         <div className="ons-grid__col ons-col-8@m ons-u-fs-m ons-u-mt-s">
-                            Showing 1 to {migrationsResp.total_count} of {migrationsResp.total_count} jobs
+                            Showing 1 to {migrationsResp.count} of {migrationsResp.total_count} jobs
                         </div>
                         <div className="ons-grid__col ons-col-2@m ons-push-1@m">
                             <LinkButton
