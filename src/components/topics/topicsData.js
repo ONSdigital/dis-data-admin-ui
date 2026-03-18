ba002f080f0c006893ef78d91a96386d8e6de865
jondewijones <jondewijones@gmail.com>
2026-03-18 19:44:07 +0000

    make getSubTopic func consistent by using map rather than forEach

--- src/components/topics/topicsData.js
+++ src/components/topics/topicsData.js
@@ -12,14 +12,14 @@ export const getAllTopics = async (reqCfg) => {
     
     const mappedTopics = await Promise.all(
         topics.items.map(async (topic) => {
-          const subTubTopicURL = new URL(topic.links.subtopics.href);
-          const subTopic = await getSubTopics(reqCfg, subTubTopicURL.pathname.substring(3));
-          return mapTopic(topic, subTopic);
+            const subTubTopicURL = new URL(topic.links.subtopics.href);
+            const subTopic = await getSubTopics(reqCfg, subTubTopicURL.pathname.substring(3));
+            return mapTopic(topic, subTopic);
         })
-      );
+    );
     
-      return mappedTopics;
-}
+    return mappedTopics;
+};
 
 /**
  * Returns mapped subtopics for a given topics.
@@ -29,14 +29,13 @@ export const getAllTopics = async (reqCfg) => {
  * @returns {<Array>}
  */
 const getSubTopics = async(reqCfg, url) => {
-    const subTopics = await httpGet(reqCfg, url)
-    if (subTopics.ok != null && !subTopics.ok || subTopics?.items.length === 0) return []
-    const mappedSubTopics = []
-    subTopics.items.forEach(subTopic => {
-        mappedSubTopics.push(mapTopic(subTopic, null))
-    })
+    const subTopics = await httpGet(reqCfg, url);
+    if (subTopics.ok != null && !subTopics.ok || subTopics?.items.length === 0) return [];
+    const mappedSubTopics = subTopics.items.map(subTopic => {
+        return mapTopic(subTopic, null);
+    });
     return mappedSubTopics;
-}
+};
 
 /**
  * Maps a raw topic record (as returned by the API) into componeent ready shape.
@@ -50,5 +49,5 @@ const mapTopic = (topic, subTopics) => {
         id: topic.id,
         label: topic.title,
         subtopics: subTopics || null
-    }
-}
+    };
+};
