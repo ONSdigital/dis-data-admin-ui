c89f173f31f75f2d26c0dadfd42dfb04491e7fd2
jondewijones <jondewijones@gmail.com>
2026-03-27 16:12:48 +0000

    clean up unused code

--- src/components/topics/Topics.js
+++ src/components/topics/Topics.js
@@ -1,17 +1,14 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Field, Button, Checkbox, Panel } from "author-design-system-react";
+import { Field, Panel } from "author-design-system-react";
 import Accordion from "../accordion/Accordion";
-import { getAllTopics, mapTopicsToTopicSelector } from "./topicsData";
 import Table from "../table/Table";
 
 export default function Topics({ listOfAllTopics, preSelectedTopics, topicsError }) {
     const [selectedTopics, setSelectedTopics] = useState(preSelectedTopics || []);
     const [topicSummary, setTopicSummary] = useState();
     const [mainTopicID, setMainTopicID] = useState(preSelectedTopics[0] || null);
-    // const [selectedTopics, setSelectedTopics] = useState(preSelectedTopics);
-    // const [checkboxOptionsItems, setCheckboxOptionsItems] = useState(createCheckboxes);
 
     const mainTopicOnChange = (topicID) => {
         setMainTopicID(topicID)
@@ -56,44 +53,6 @@ export default function Topics({ listOfAllTopics, preSelectedTopics, topicsError
     useEffect(() => {
         setTopicSummary(mapTopicSummaryTable(selectedTopics));
     }, [selectedTopics, mainTopicID]);
-
-    // useEffect(() => {
-    //     /* eslint-disable-next-line react-hooks/set-state-in-effect */
-    //     setCheckboxOptionsItems(createCheckboxes);
-    // }, [selectedTopics]);
-
-    // useEffect(() => {
-    //     checkboxOptionsItems.itemsList.forEach(checkboxOption => {
-    //         const checkbox = document.getElementById(checkboxOption.id);
-    //         if (checkboxOption.checked === false) {
-    //             checkbox.defaultChecked = false;
-    //         }
-    //     });
-    // }, [checkboxOptionsItems]);
-
-
-    // function createCheckboxes() {
-    //     const checkboxOptions = [];
-    //     listOfAllTopics.forEach(topic => {
-    //         const topicId = topic.current?.id || topic.next?.id || topic.id || "missing id";
-    //         const topicTitle = topic.current?.title || topic.next?.title || topic.title || "missing title";
-    //         const selected = selectedTopics.includes(topicId);
-
-    //         if (topicId && topicTitle) {
-    //             checkboxOptions.push({
-    //                 dataTestId: "checkbox-" + topicId,
-    //                 id: "checkbox-" + topicId,
-    //                 label: {
-    //                     text: topicTitle
-    //                 },
-    //                 onChange: () => { topicOnChange(topicId); },
-    //                 value: topicId,
-    //                 checked: selected
-    //             });
-    //         }
-    //     });
-    //     return ({ itemsList: checkboxOptions });
-    // }
 
     const topicOnChange = (topic) => {
         // while no topics are curerently selected assume the first topic
@@ -153,37 +112,5 @@ export default function Topics({ listOfAllTopics, preSelectedTopics, topicsError
                 <Table contents={topicSummary} noResultsText="No topic selected" />
             </Field>
         </div>
-    );
-
-    return (
-        <>
-            <Field dataTestId="field-dataset-series-topics" error={topicsError ? { id: "dataset-series-topics-error", text: topicsError } : null}>
-                <input id="dataset-series-topics-input" type="hidden" name="dataset-series-topics-input" value={JSON.stringify(selectedTopics)} />
-                <Checkbox
-                    id="dataset-series-topics"
-                    dataTestId="dataset-series-topics-checkbox"
-                    items={checkboxOptionsItems}
-                    legend="Topics"
-                    borderless
-                    classes="ons-u-mb-m"
-                />
-                <Button
-                    id="dataset-series-clear-topic-selection-button"
-                    dataTestId="dataset-series-clear-topic-selection-button"
-                    text="Clear Selection"
-                    variants={[
-                        "secondary",
-                        "small"
-                    ]}
-                    onClick={() => {
-                        const inputs = document.querySelectorAll("input[type='checkbox']");
-                        for (let i = 0; i < inputs.length; i++) {
-                            inputs[i].checked = false;
-                        }
-                        setSelectedTopics([]);
-                    }}
-                />
-            </Field>
-        </>
     );
 }
