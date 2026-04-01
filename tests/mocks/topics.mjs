c25047e6cb47f214fdcba51094a297035baf4bb6
jondewijones <jondewijones@gmail.com>
2026-04-01 20:37:43 +0100

    add subtopics data to mocks

--- tests/mocks/topics.mjs
+++ tests/mocks/topics.mjs
@@ -2,11 +2,45 @@ export const topicList = {
     "items": [
         {
             "id": "1000",
-            "title": "Business, industry and trade"
+            "title": "Business",
+            "links": {
+                "subtopics": {
+                    "href": "https://api.dp.aws.onsdigital.uk/v1/topics/1000/subtopics"
+                }
+            },
+            "subtopics": {
+                "items": [
+                    {
+                        "id": "1001",
+                        "title": "Business sub topic 1"
+                    },
+                    {
+                        "id": "1002",
+                        "title": "Business sub topic 2"
+                    },
+                ]
+            }
         },
         {
-            "id": "1001",
-            "title": "Census"
+            "id": "2000",
+            "title": "Census",
+            "links": {
+                "subtopics": {
+                    "href": "https://api.dp.aws.onsdigital.uk/v1/topics/2000/subtopics"
+                }
+            },
+            "subtopics": {
+                "items": [
+                    {
+                        "id": "2001",
+                        "title": "Census sub topic 1"
+                    },
+                    {
+                        "id": "2002",
+                        "title": "Census sub topic 2"
+                    },
+                ]
+            }
         }
     ]
 }
