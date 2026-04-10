4fad78a29bb686ba40d9ef55cf13cf2431df4843
JoshSinghONS <joshua.singh@ons.gov.uk>
2026-04-10 12:16:01 +0100

    Changed config to use env variable from secrets

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import("next").NextConfig} */
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? [process.env.ALLOWED_ORIGINS] : ["localhost:29500"];
+    
 module.exports = {
   output: "standalone",
   basePath: "/data-admin",
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        "publishing.dp.aws.onsdigital.uk", // sandbox
-        "publishing.dp-staging.aws.onsdigital.uk", // staging
-        "localhost:29500" // local when running in dataset-catalogue stack
-      ],
+      allowedOrigins
     },
   },
 };
