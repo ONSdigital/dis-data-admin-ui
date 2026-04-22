ff465d72ba467c2f7fa1b571ff303f002c754517
JoshSinghONS <joshua.singh@ons.gov.uk>
2026-04-22 14:26:23 +0100

    Debugging

--- next.config.js
+++ next.config.js
@@ -7,7 +7,14 @@ module.exports = {
   basePath: "/data-admin",
   experimental: {
     serverActions: {
-      allowedOrigins
+      allowedOrigins: [
+        "publishing.dp.aws.onsdigital.uk", // sandbox
+        "publishing.dp-staging.aws.onsdigital.uk", // staging
+        "localhost:29500" // local when running in dataset-catalogue stack
+      ],
     },
   },
 };
+
+console.log(module.exports.experimental.serverActions.allowedOrigins)
+console.log(allowedOrigins)
