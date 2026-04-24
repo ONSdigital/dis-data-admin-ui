c2e2807a2e1c073b5231313de216fecf1bf71540
JoshSinghONS <joshua.singh@ons.gov.uk>
2026-04-24 11:05:46 +0100

    Moved getting env varibales to inside module.exports

--- next.config.js
+++ next.config.js
@@ -1,21 +1,14 @@
 /** @type {import("next").NextConfig} */
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",").map(s => s.trim()) : ["localhost:29500"];
-
 module.exports = {
   output: "standalone",
   basePath: "/data-admin",
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        "publishing.dp.aws.onsdigital.uk", // sandbox
-        "publishing.dp-staging.aws.onsdigital.uk", // staging
-        "localhost:29500" // local when running in dataset-catalogue stack
-      ],
+      allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : ["localhost:29500"],
     },
   },
   allowedDevOrigins: ["127.0.0.1"], // for component tests
 };
 
-console.log("Allowed origins from env vars: ", allowedOrigins);
 console.log("Allowed orgings from module.exports: ", module.exports.experimental.serverActions.allowedOrigins)
