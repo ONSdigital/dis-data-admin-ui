28a4aa48a482cdbbf0a9c5b2580c69ada4653ac4
JoshSinghONS <joshua.singh@ons.gov.uk>
2026-04-27 11:57:12 +0100

    Revert back to hardcoded values for allowed origins

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import("next").NextConfig} */
-
 module.exports = {
   output: "standalone",
   basePath: "/data-admin",
   experimental: {
     serverActions: {
-      allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : ["localhost:29500"],
+      allowedOrigins: [
+        "publishing.dp.aws.onsdigital.uk", // sandbox
+        "publishing.dp-staging.aws.onsdigital.uk", // staging
+        "localhost:29500" // local when running in dataset-catalogue stack
+      ],
     },
   },
-  allowedDevOrigins: ["127.0.0.1"], // for component tests
 };
-
-console.log("Allowed orgings from module.exports: ", module.exports.experimental.serverActions.allowedOrigins)
