0206813627b4f34bd97fd9a5e41297ce06df2827
JoshSinghONS <joshua.singh@ons.gov.uk>
2026-06-15 09:08:40 +0100

    Updated button label

--- src/app/migration/[id]/reject/[associatedDataset]/page.js
+++ src/app/migration/[id]/reject/[associatedDataset]/page.js
@@ -12,7 +12,7 @@ export default async function reject({ params }) {
             <StateChangeButton
                 dataTestId="migration-reject-button"
                 id="migration-reject-button"
-                text="Reject"
+                text="Reject Migration"
                 jobID={id}
                 jobState={"rejected"}
                 series={associatedDataset}
