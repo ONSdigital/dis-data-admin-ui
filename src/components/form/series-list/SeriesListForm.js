ea51a21a0547b730a5776d1aeb3a9d7b70a5f595
jondewijones <jondewijones@gmail.com>
2026-07-09 17:47:30 +0100

    make series list filter form consistent with migration filter form by use box container

--- src/components/form/series-list/SeriesListForm.js
+++ src/components/form/series-list/SeriesListForm.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-import { TextInput } from "author-design-system-react";
+import { BoxContainer, TextInput } from "@/components/design-system/DesignSystem";
 
 export default function SeriesListForm({datasetID}) {
     const [id, setID] = useState(datasetID || "");
@@ -16,14 +16,23 @@ export default function SeriesListForm({datasetID}) {
         push(`/series?id=${id}`);
     };
     return (
-        <>    
-            <TextInput id="series-list-search-by-id" 
-                dataTestId="series-list-search-by-id" 
-                label={{text: "Search by ID", description: "E.g \"CPIH01\" or \"my-dataset-id\""}}
-                value={id}
-                onChange={e => setID(e.target.value)}
-                searchButton={{text: "Search", onClick: handleSearchCLick}} 
-            />
-        </>
+        <BoxContainer
+            borderColor="ons-color-grey-15"
+            borderWidth={1}
+            classes="ons-grid__col ons-u-pl-no"
+            id="box-container"
+            title="Filter results"
+        > 
+            <div className="ons-u-mt-xs">
+                <TextInput id="series-list-search-by-id" 
+                    dataTestId="series-list-search-by-id" 
+                    label={{text: "Search by ID", description: "E.g \"CPIH01\" or \"my-dataset-id\""}}
+                    value={id}
+                    classes="ons-input--w-20"
+                    onChange={e => setID(e.target.value)}
+                    searchButton={{text: "Search", onClick: handleSearchCLick}} 
+                />
+            </div>
+        </BoxContainer>
     );
 }
