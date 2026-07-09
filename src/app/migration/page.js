c608735a7f2bb4e1614e891f609e04db835a64d1
jondewijones <jondewijones@gmail.com>
2026-07-09 18:57:58 +0100

    remove debug console log

--- src/app/migration/page.js
+++ src/app/migration/page.js
@@ -25,8 +25,6 @@ export default async function MigrationList({ searchParams }) {
         migrationsRespError = true;
     }
 
-    console.log(migrationsResp);
-
     if (migrationsRespError) {
         return (
             <Panel title="Error" variant="error" dataTestId="migrations-list-response-error">
