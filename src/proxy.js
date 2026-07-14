91bac530c204e82d339af01155abf05527494425
jondewijones <jondewijones@gmail.com>
2026-07-14 20:31:20 +0100

    update insteance of 'middleware' to 'proxy'

--- src/proxy.js
+++ src/proxy.js
@@ -3,13 +3,13 @@ import { authenticationProxy } from "./proxy/authentication";
 import { routeLoggingProxy } from "./proxy/routeLogging";
 import { urlValueHeaders } from "./proxy/urlValueHeaders";
 
-export const middleware = multipleProxies([
+export const proxy = multipleProxies([
     routeLoggingProxy,
     authenticationProxy,
     urlValueHeaders
 ]);
 
-// applies this middleware only to files in the app directory
+// applies this proxy only to files in the app directory
 export const config = {
     matcher: "/((?!api|static|assets|robots|sitemap|sw|service-worker|manifest|.*\\..*|_next).*)",
 };
