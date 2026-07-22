8d37af0e5bdad88752b084c03a25a6a25191e115
jondewijones <jondewijones@gmail.com>
2026-07-22 16:22:05 +0100

    fix formatting

--- tests/mocks/datasets.mjs
+++ tests/mocks/datasets.mjs
@@ -168,21 +168,21 @@ export const datasetList = {
             "title": "Foobar test dataset",
             "description": "A test dataset",
             "id": "foo-bar"
-          },
-          {
+        },
+        {
             "next": {
-              "title": "Test associated",
-              "description": "A test dataset",
-              "id": "test-publish-message-dataset",
-              "state": "associated"
+                "title": "Test associated",
+                "description": "A test dataset",
+                "id": "test-publish-message-dataset",
+                "state": "associated"
             },
             "current": {
-              "title": "Test published",
-              "description": "A test dataset",
-              "id": "test-publish-message-dataset",
-              "state": "published"
+                "title": "Test published",
+                "description": "A test dataset",
+                "id": "test-publish-message-dataset",
+                "state": "published"
             }
-          }
+        }
     ],
     "count": 20,
     "total_count": 130
