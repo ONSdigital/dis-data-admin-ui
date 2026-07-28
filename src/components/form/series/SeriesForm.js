92b58f1a473e015f138d71bcb1d09ee08a01c9e7
aryan-patel <aryanpatel1771@gmail.com>
2026-07-28 14:06:15 +0100

    change  to  on series form submission

--- src/components/form/series/SeriesForm.js
+++ src/components/form/series/SeriesForm.js
@@ -64,7 +64,7 @@ export default function SeriesForm({ currentTitle = "", currentID = "", currentD
             {renderFailure()}
             <form className="ons-u-mt-m" action={formAction}>
                 <input id="dataset-series-type" name="dataset-series-type" type="hidden" value="static" />
-                <input id="dataset-series-license" name="dataset-series-license" type="hidden" value="Open Government License v3.0" />
+                <input id="dataset-series-license" name="dataset-series-license" type="hidden" value="Open Government Licence v3.0" />
                 {isPublished && <input id="dataset-series-id" data-testid="dataset-series-id" name="dataset-series-id" type="hidden" value={id} />}
                 {!isPublished &&
                     <TextInput
