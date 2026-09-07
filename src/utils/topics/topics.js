0eeca7d038ef3ba0334a1866991bbe3003463850
jondewijones <jondewijones@gmail.com>
2026-09-08 00:56:58 +0100

    update to use getTopic instead of wrongly getting subtopics

--- src/utils/topics/topics.js
+++ src/utils/topics/topics.js
@@ -1,21 +1,23 @@
 import { logError } from "../log/log";
-import { httpGet } from "@/utils/request/request";
+import { getTopic } from "@/utils/request/api-clients/topics";
 
-export function convertTopicIDsToTopicTitles(topicIDs, reqCfg) {
+export function convertTopicIDsToTopicTitles(topicIDs, accessToken) {
     if (!topicIDs || topicIDs.length === 0) {
         return null;
     }
 
     return Promise.all(
-        topicIDs.map((topicID) => getTopicTitle(topicID, reqCfg))
+        topicIDs.map((topicID) => getTopicTitle(topicID, accessToken))
     );
 }
 
-export async function getTopicTitle(topicID, reqCfg) {
+export async function getTopicTitle(topicID, accessToken) {
     try {
-        const response = await httpGet(reqCfg, `/topics/${topicID}`);
-        return response?.current?.title || response?.next?.title || response?.title || `${topicID} - unable to find topic title`;
+        const topic = await getTopic(topicID, accessToken);
+        console.log("TOPIC", topic);
+        return topic?.response?.current?.title || topic?.response?.next?.title || topic?.response?.title || `${topicID} - unable to find topic title`;
     } catch (error) {
+        console.log("in here")
         logError("error fetching topic", topicID, null, error);
         return `${topicID} - unable to find topic title`;
     }
