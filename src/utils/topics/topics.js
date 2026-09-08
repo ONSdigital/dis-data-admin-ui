998da5c9422c317b1e45b329e1f28387bec5304a
jondewijones <jondewijones@gmail.com>
2026-09-08 16:24:10 +0100

    remove debug console.logs

--- src/utils/topics/topics.js
+++ src/utils/topics/topics.js
@@ -14,10 +14,8 @@ export function convertTopicIDsToTopicTitles(topicIDs, accessToken) {
 export async function getTopicTitle(topicID, accessToken) {
     try {
         const topic = await getTopic(topicID, accessToken);
-        console.log("TOPIC", topic);
         return topic?.response?.current?.title || topic?.response?.next?.title || topic?.response?.title || `${topicID} - unable to find topic title`;
     } catch (error) {
-        console.log("in here")
         logError("error fetching topic", topicID, null, error);
         return `${topicID} - unable to find topic title`;
     }
