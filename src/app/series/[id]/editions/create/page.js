650d40557428e7bbf845eebc419ee4d2c53f6716
jondewijones <jondewijones@gmail.com>
2026-09-08 22:02:21 +0100

    remove use of SSRequestConfig which has been removed

--- src/app/series/[id]/editions/create/page.js
+++ src/app/series/[id]/editions/create/page.js
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 
 import { createDatasetEdition } from "@/app/actions/datasetEdition";
-import { SSRequestConfig } from "@/utils/request/request";
+import { getAcessTokenFromCookie } from "@/utils/auth/auth";
 
 import { Panel } from "@/components/design-system/DesignSystem";
 import EditionForm from "@/components/form/edition/EditionForm";
@@ -9,8 +9,7 @@ import PageHeading from "@/components/page-heading/PageHeading";
 
 export default async function CreateEditionPage({ params }) {
     const { id } = await params;
-    const reqCfg = await SSRequestConfig(cookies);
-    const accessToken = reqCfg.authToken;
+    const accessToken = getAcessTokenFromCookie(cookies);
 
     return (
         <>
